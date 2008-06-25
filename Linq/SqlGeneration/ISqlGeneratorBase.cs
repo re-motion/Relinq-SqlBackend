a283d702dae74114911f2357296d70a453c83260
Matthias Goertler <matthias.goertler@rubicon.eu>
2008-06-25 12:33:51 +0000

    added using of expressionTreeRoot

--- Linq/SqlGeneration/ISqlGeneratorBase.cs
+++ Linq/SqlGeneration/ISqlGeneratorBase.cs
@@ -1,9 +1,11 @@
 using Remotion.Collections;
+using Remotion.Data.Linq.Parsing.Details;
 
 namespace Remotion.Data.Linq.SqlGeneration
 {
   public interface ISqlGeneratorBase
   {
     Tuple<string, CommandParameter[]> BuildCommandString (QueryModel queryModel);
+    DetailParser DetailParser { get; }
   }
 }
