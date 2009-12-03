0616874d0d028a14a6d09e45522e8c7ae3fa7b0c
Michael Ketting <michael.ketting@rubicon.eu>
2009-12-03 08:45:15 +0000

    Moved ObservableCollection to re-linq. (RM-1950)

--- Remotion/Data/Linq/Backend/SqlGeneration/SqlGeneratorVisitor.cs
+++ Remotion/Data/Linq/Backend/SqlGeneration/SqlGeneratorVisitor.cs
@@ -17,10 +17,10 @@
 using System;
 using System.Collections.Generic;
 using System.Linq.Expressions;
-using Remotion.Collections;
 using Remotion.Data.Linq.Backend.DataObjectModel;
 using Remotion.Data.Linq.Backend.DetailParsing;
 using Remotion.Data.Linq.Clauses;
+using Remotion.Data.Linq.Collections;
 using Remotion.Data.Linq.Utilities;
 
 namespace Remotion.Data.Linq.Backend.SqlGeneration
