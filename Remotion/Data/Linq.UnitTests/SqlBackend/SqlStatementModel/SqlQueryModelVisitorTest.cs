f440e8e7d08e9317b58fe8149ebaa9dcead3b24a
Andreas Obkircher <andreas.obkircher@rubicon.eu>
2010-02-22 13:54:17 +0000

    added SqlStatement which represents the sql backend model added SqlQueryModelVisitor to transform QueryModel to SqlStatement added FromExpression to SqlStatement added SelectProjection to SqlStatement (RM-2315)

---
+++ Remotion/Data/Linq.UnitTests/SqlBackend/SqlStatementModel/SqlQueryModelVisitorTest.cs
@@ -0,0 +1,63 @@
+// This file is part of the re-motion Core Framework (www.re-motion.org)
+// Copyright (C) 2005-2009 rubicon informationstechnologie gmbh, www.rubicon.eu
+// 
+// The re-motion Core Framework is free software; you can redistribute it 
+// and/or modify it under the terms of the GNU Lesser General Public License 
+// as published by the Free Software Foundation; either version 2.1 of the 
+// License, or (at your option) any later version.
+// 
+// re-motion is distributed in the hope that it will be useful, 
+// but WITHOUT ANY WARRANTY; without even the implied warranty of 
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 
+// GNU Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License
+// along with re-motion; if not, see http://www.gnu.org/licenses.
+// 
+using System;
+using System.Linq.Expressions;
+using NUnit.Framework;
+using NUnit.Framework.SyntaxHelpers;
+using Remotion.Data.Linq.Clauses;
+using Remotion.Data.Linq.Clauses.Expressions;
+using Remotion.Data.Linq.SqlBackend.SqlStatementModel;
+using Remotion.Data.Linq.UnitTests.TestDomain;
+
+namespace Remotion.Data.Linq.UnitTests.SqlBackend.SqlStatementModel
+{
+  [TestFixture]
+  public class SqlQueryModelVisitorTest
+  {
+    private SelectClause _selectClause;
+    private MainFromClause _mainFromClause;
+    private QueryModel _queryModel;
+    private SqlQueryModelVisitor _sqlQueryModelVisitor;
+    
+    [SetUp]
+    public void SetUp ()
+    {
+      _mainFromClause = new MainFromClause ("x", typeof (Student), Expression.Constant ("source"));
+      _selectClause = new SelectClause (new QuerySourceReferenceExpression (_mainFromClause));
+      _queryModel = new QueryModel (_mainFromClause, _selectClause);
+      _sqlQueryModelVisitor = new SqlQueryModelVisitor ();
+    }
+
+    [Test]
+    public void VisitSelectClause_CreatesSelectProjection ()
+    {
+      _sqlQueryModelVisitor.VisitSelectClause (_selectClause, _queryModel);
+
+      Assert.That (_sqlQueryModelVisitor.SqlStatement.SelectProjection, Is.Not.Null);
+      Assert.That (_sqlQueryModelVisitor.SqlStatement.SelectProjection.Expression, Is.SameAs (_selectClause.Selector));
+    }
+
+    [Test]
+    public void VisitFromClause_CreatesFromExpression ()
+    {
+      _sqlQueryModelVisitor.VisitMainFromClause (_mainFromClause, _queryModel);
+      
+      Assert.That (_sqlQueryModelVisitor.SqlStatement.FromExpression, Is.Not.Null);
+      Assert.That (_sqlQueryModelVisitor.SqlStatement.FromExpression.Expression, Is.SameAs (_mainFromClause.FromExpression));
+    }
+  }
+}
