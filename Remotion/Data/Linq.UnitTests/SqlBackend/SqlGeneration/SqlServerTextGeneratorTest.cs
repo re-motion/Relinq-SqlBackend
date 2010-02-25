11487940e4572056f4ac524ba8c9769933ef2a77
Andreas Obkircher <andreas.obkircher@rubicon.eu>
2010-02-25 08:53:30 +0000

    added SqlServerTextGenerator to generate sql-text for sql server added tests (RM-2323)

---
+++ Remotion/Data/Linq.UnitTests/SqlBackend/SqlGeneration/SqlServerTextGeneratorTest.cs
@@ -0,0 +1,54 @@
+// This file is part of the re-motion Core Framework (www.re-motion.org)
+// Copyright (C) 2005-2009 rubicon informationstechnologie gmbh, www.rubicon.eu
+// 
+// The re-motion Core Framework is free software; you can redistribute it 
+// and/or modify it under the terms of the GNU Lesser General Public License 
+// as published by the Free Software Foundation; either version 2.1 of the 
+// License, or (at your option) any later version.
+// 
+// re-motion is distributed in the hope that it will be useful, 
+// but WITHOUT ANY WARRANTY; without even the implied warranty of 
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 
+// GNU Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License
+// along with re-motion; if not, see http://www.gnu.org/licenses.
+// 
+using System;
+using NUnit.Framework;
+using NUnit.Framework.SyntaxHelpers;
+using Remotion.Data.Linq.SqlBackend.SqlGeneration;
+using Remotion.Data.Linq.SqlBackend.SqlStatementModel;
+using Remotion.Data.Linq.UnitTests.SqlBackend.SqlStatementModel;
+
+namespace Remotion.Data.Linq.UnitTests.SqlBackend.SqlGeneration
+{
+  [TestFixture]
+  public class SqlServerTextGeneratorTest
+  {
+    [Test]
+    public void Build ()
+    {
+      SqlStatement sqlStatement = new SqlStatement();
+      
+      var sqlTable = SqlStatementModelObjectMother.CreateSqlTable ();
+      sqlTable.TableSource = new SqlTableSource (typeof (int), "Table", "t");
+      var tableReferenceExpression = new SqlTableReferenceExpression (sqlTable);
+      var columnListExpression = new SqlColumnListExpression (
+          tableReferenceExpression.Type,
+          new[]
+          {
+              new SqlColumnExpression (typeof (int), sqlTable, "ID"),
+              new SqlColumnExpression (typeof (int), sqlTable, "Name"),
+              new SqlColumnExpression (typeof (int), sqlTable, "City")
+          });
+
+      sqlStatement.SelectProjection = columnListExpression;
+      sqlStatement.FromExpression.TableSource = sqlTable.TableSource;
+
+      SqlServerTextGenerator generator = new SqlServerTextGenerator();
+      var result = generator.Build (sqlStatement);
+      Assert.That (result, Is.EqualTo ("SELECT [t].[ID],[t].[Name],[t].[City] FROM [Table] AS [t]"));
+    }
+  }
+}
