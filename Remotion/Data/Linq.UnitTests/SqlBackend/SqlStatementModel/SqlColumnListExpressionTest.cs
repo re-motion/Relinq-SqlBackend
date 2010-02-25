64432359a7fd3b23e250ce80c47b7999ac2e9cb5
Andreas Obkircher <andreas.obkircher@rubicon.eu>
2010-02-25 12:30:26 +0000

    added missing test for SqlColumnListExpression.VisitChildren (RM-2343)

--- Remotion/Data/Linq.UnitTests/SqlBackend/SqlStatementModel/SqlColumnListExpressionTest.cs
+++ Remotion/Data/Linq.UnitTests/SqlBackend/SqlStatementModel/SqlColumnListExpressionTest.cs
@@ -18,10 +18,9 @@ using System;
 using System.Linq.Expressions;
 using NUnit.Framework;
 using NUnit.Framework.SyntaxHelpers;
-using Remotion.Data.Linq.SqlBackend.MappingResolution;
-using Remotion.Data.Linq.SqlBackend.SqlGeneration;
+using Remotion.Data.Linq.Parsing;
 using Remotion.Data.Linq.SqlBackend.SqlStatementModel;
-using Remotion.Data.Linq.UnitTests.SqlBackend.MappingResolution;
+using Remotion.Data.Linq.UnitTests.TestUtilities;
 using Rhino.Mocks;
 
 namespace Remotion.Data.Linq.UnitTests.SqlBackend.SqlStatementModel
@@ -30,24 +29,23 @@ namespace Remotion.Data.Linq.UnitTests.SqlBackend.SqlStatementModel
   public class SqlColumnListExpressionTest
   {
     private SqlColumnListExpression _columnListExpression;
+    private SqlColumnExpression _columnExpression1;
+    private SqlColumnExpression _columnExpression2;
+    private SqlColumnExpression _columnExpression3;
+    private SqlTableReferenceExpression _tableReferenceExpression;
 
     [SetUp]
     public void SetUp ()
     {
-      var resolver = new SqlStatementResolverStub();
       var source = new ConstantTableSource (Expression.Constant ("Student", typeof (string)));
-      var sqlTable = new SqlTable ();
+      var sqlTable = new SqlTable();
       sqlTable.TableSource = source;
-      var tableReferenceExpression = new SqlTableReferenceExpression (sqlTable);
-
+      _tableReferenceExpression = new SqlTableReferenceExpression (sqlTable);
+      _columnExpression1 = new SqlColumnExpression (typeof (int), "t", "ID");
+      _columnExpression2 = new SqlColumnExpression (typeof (int), "t", "Name");
+      _columnExpression3 = new SqlColumnExpression (typeof (int), "t", "City");
       _columnListExpression = new SqlColumnListExpression (
-          tableReferenceExpression.Type,
-          new[]
-          {
-              new SqlColumnExpression (typeof (int), "t", "ID"),
-              new SqlColumnExpression (typeof (int), "t", "Name"),
-              new SqlColumnExpression (typeof (int), "t", "City")
-          });
+          _tableReferenceExpression.Type, new[] { _columnExpression1, _columnExpression2, _columnExpression3 });
     }
 
     [Test]
@@ -57,6 +55,36 @@ namespace Remotion.Data.Linq.UnitTests.SqlBackend.SqlStatementModel
       Assert.That (expression, Is.SameAs (_columnListExpression));
     }
 
+    [Test]
+    public void VisitChildren_NoColumnChanged ()
+    {
+      var visitorMock = MockRepository.GenerateMock<ExpressionTreeVisitor>();
+      visitorMock.Expect (mock => mock.VisitExpression (_columnExpression1)).Return (_columnExpression1);
+      visitorMock.Expect (mock => mock.VisitExpression (_columnExpression2)).Return (_columnExpression2);
+      visitorMock.Expect (mock => mock.VisitExpression (_columnExpression3)).Return (_columnExpression3);
+      visitorMock.Replay();
+      var expression = PrivateInvoke.InvokeNonPublicMethod (_columnListExpression, "VisitChildren", visitorMock);
+      
+      Assert.That (expression, Is.SameAs (_columnListExpression));
+    }
 
+    [Test]
+    public void VisitChildren_ChangeColumn ()
+    {
+      var newColumnExpression = new SqlColumnExpression (typeof (string), "o", "Test");
+
+      var visitorMock = MockRepository.GenerateMock<ExpressionTreeVisitor> ();
+      visitorMock.Expect (mock => mock.VisitExpression (_columnExpression1)).Return (_columnExpression1);
+      visitorMock.Expect (mock => mock.VisitExpression (_columnExpression2)).Return (newColumnExpression);
+      visitorMock.Expect (mock => mock.VisitExpression (_columnExpression3)).Return (_columnExpression3);
+      visitorMock.Replay ();
+      var expression = (SqlColumnListExpression) PrivateInvoke.InvokeNonPublicMethod (_columnListExpression, "VisitChildren", visitorMock);
+
+      var expectedColumnListExpression = new SqlColumnListExpression (
+          _tableReferenceExpression.Type, new[] { _columnExpression1, newColumnExpression, _columnExpression3 });
+
+      Assert.That (expression, Is.Not.EqualTo (_columnListExpression));
+      Assert.That (expression.Columns, Is.EqualTo (expectedColumnListExpression.Columns));
+    }
   }
 }
