940d720f08eba58dd3dfb97e5fff2283ea75dd1d
Andreas Obkircher <andreas.obkircher@rubicon.eu>
2010-03-09 11:24:42 +0000

    changed ResolvingTableSourceVisitor.ResolveConstantTableSource and ResolvingTableSourceVisitor.ResolveJoinedTableSource to resolve table sources until the same tablesource is returned. added tests. (RM-2411)

--- Remotion/Data/Linq.UnitTests/SqlBackend/MappingResolution/ResolvingTableSourceVisitorTest.cs
+++ Remotion/Data/Linq.UnitTests/SqlBackend/MappingResolution/ResolvingTableSourceVisitorTest.cs
@@ -15,6 +15,7 @@
 // along with re-motion; if not, see http://www.gnu.org/licenses.
 // 
 using System;
+using System.Linq.Expressions;
 using NUnit.Framework;
 using NUnit.Framework.SyntaxHelpers;
 using Remotion.Data.Linq.SqlBackend.MappingResolution;
@@ -30,48 +31,128 @@ namespace Remotion.Data.Linq.UnitTests.SqlBackend.MappingResolution
   [TestFixture]
   public class ResolvingTableSourceVisitorTest
   {
+    private ISqlStatementResolver _resolverMock;
+    private SqlTable _constantSqlTable;
+    private SqlTable _joinedSqlTable;
+
+    [SetUp]
+    public void SetUp ()
+    {
+      _resolverMock = MockRepository.GenerateMock<ISqlStatementResolver>();
+      _constantSqlTable = SqlStatementModelObjectMother.CreateSqlTableWithConstantTableSource();
+      _joinedSqlTable = SqlStatementModelObjectMother.CreateSqlTableWithJoinedTableSource();
+    }
+
     [Test]
     public void ResolveConstantTableSource ()
     {
-      var sqlTable = SqlStatementModelObjectMother.CreateSqlTableWithConstantTableSource();
-      var resolver = MockRepository.GenerateMock<ISqlStatementResolver>();
-
       var tableSource = new SqlTableSource (typeof (int), "Table", "t");
-      resolver.Expect (mock => mock.ResolveConstantTableSource ((ConstantTableSource) sqlTable.TableSource)).Return (tableSource);
+      _resolverMock.Expect (mock => mock.ResolveConstantTableSource ((ConstantTableSource) _constantSqlTable.TableSource)).Return (tableSource);
 
-      var result = ResolvingTableSourceVisitor.ResolveTableSource (tableSource, resolver);
+      var result = ResolvingTableSourceVisitor.ResolveTableSource (tableSource, _resolverMock);
 
       Assert.That (result, Is.TypeOf (typeof (SqlTableSource)));
+    }
+
+    [Test]
+    public void ResolveConstantTableSource_AndRevisitsResult ()
+    {
+      var unresolvedResult = new ConstantTableSource (Expression.Constant (0), typeof (int));
+      var resolvedResult = new SqlTableSource (typeof (int), "Table", "t");
+
+      using (_resolverMock.GetMockRepository().Ordered())
+      {
+        _resolverMock
+            .Expect (mock => mock.ResolveConstantTableSource ((ConstantTableSource) _constantSqlTable.TableSource))
+            .Return (unresolvedResult);
+        _resolverMock
+            .Expect (mock => mock.ResolveConstantTableSource (unresolvedResult))
+            .Return (resolvedResult);
+      }
+
+      var result = ResolvingTableSourceVisitor.ResolveTableSource (_constantSqlTable.TableSource, _resolverMock);
+
+      Assert.That (result, Is.SameAs (resolvedResult));
+      _resolverMock.VerifyAllExpectations();
+    }
+
+    [Test]
+    public void ResolveConstantTableSource_AndRevisitsResult_OnlyIfDifferent ()
+    {
+      _resolverMock
+          .Expect (mock => mock.ResolveConstantTableSource ((ConstantTableSource) _constantSqlTable.TableSource))
+          .Return (_constantSqlTable.TableSource);
+
+      var result = ResolvingTableSourceVisitor.ResolveTableSource (_constantSqlTable.TableSource, _resolverMock);
+
+      Assert.That (result, Is.SameAs (_constantSqlTable.TableSource));
+      _resolverMock.VerifyAllExpectations();
     }
 
     [Test]
     [ExpectedException (typeof (NotImplementedException))]
     public void ResolveConstantTableSource_WithUnknownTableSource ()
     {
-      var sqlTable = new SqlTable(new UnknownTableSource());
-      var resolver = MockRepository.GenerateMock<ISqlStatementResolver>();
-      ResolvingTableSourceVisitor.ResolveTableSource (sqlTable.TableSource, resolver);
+      var sqlTable = new SqlTable (new UnknownTableSource());
+      ResolvingTableSourceVisitor.ResolveTableSource (sqlTable.TableSource, _resolverMock);
     }
 
     [Test]
     public void ResolveJoinedTableSource ()
     {
-      var joinedTable = SqlStatementModelObjectMother.CreateSqlTableWithJoinedTableSource();
-      var resolver = MockRepository.GenerateMock<ISqlStatementResolver>();
-
       var kitchenSource = new SqlTableSource (typeof (Kitchen), "Kitchen", "k");
-      var cookSource = new SqlTableSource(typeof(string), "Cook", "c");
+      var cookSource = new SqlTableSource (typeof (string), "Cook", "c");
       var primaryColumn = new SqlColumnExpression (typeof (int), "k", "ID");
       var foreignColumn = new SqlColumnExpression (typeof (int), "c", "KitchenID");
 
       var sqlJoinedTableSource = new SqlJoinedTableSource (cookSource, primaryColumn, foreignColumn);
 
-      resolver.Expect (mock => mock.ResolveJoinedTableSource (Arg<JoinedTableSource>.Is.Anything)).Return (sqlJoinedTableSource);
+      _resolverMock.Expect (mock => mock.ResolveJoinedTableSource (Arg<JoinedTableSource>.Is.Anything)).Return (sqlJoinedTableSource);
 
-      var result = ResolvingTableSourceVisitor.ResolveTableSource (joinedTable.TableSource, resolver);
+      var result = ResolvingTableSourceVisitor.ResolveTableSource (_joinedSqlTable.TableSource, _resolverMock);
 
       Assert.That (result, Is.TypeOf (typeof (SqlJoinedTableSource)));
     }
+
+    [Test]
+    public void ResolveJoinedTableSource_AndRevisitsResult ()
+    {
+      var memberInfo = typeof (Cook).GetProperty ("Substitution");
+      var unresolvedResult = new JoinedTableSource (memberInfo);
+
+      var foreignTableSource = new SqlTableSource (typeof (Cook), "CookTable", "s");
+      var resolvedResult = new SqlJoinedTableSource (
+          foreignTableSource, new SqlColumnExpression (typeof (int), "c", "ID"), new SqlColumnExpression (typeof (int), "s", "ID"));
+
+      using (_resolverMock.GetMockRepository().Ordered())
+      {
+        _resolverMock
+            .Expect (mock => mock.ResolveJoinedTableSource ((JoinedTableSource) _joinedSqlTable.TableSource))
+            .Return (unresolvedResult);
+        _resolverMock
+            .Expect (mock => mock.ResolveJoinedTableSource (unresolvedResult))
+            .Return (resolvedResult);
+      }
+
+      var result = ResolvingTableSourceVisitor.ResolveTableSource (_joinedSqlTable.TableSource, _resolverMock);
+
+      Assert.That (result, Is.SameAs (resolvedResult));
+      _resolverMock.VerifyAllExpectations();
+    }
+
+    [Test]
+    public void ResolveJoinedTableSource_AndRevisitsResult_OnlyIfDifferent ()
+    {
+      _resolverMock
+          .Expect (mock => mock.ResolveJoinedTableSource ((JoinedTableSource) _joinedSqlTable.TableSource))
+          .Return (_joinedSqlTable.TableSource);
+     
+      var result = ResolvingTableSourceVisitor.ResolveTableSource (_joinedSqlTable.TableSource, _resolverMock);
+
+      Assert.That (result, Is.SameAs (_joinedSqlTable.TableSource));
+      _resolverMock.VerifyAllExpectations ();
+    }
+
 
     private class UnknownTableSource : AbstractTableSource
     {
