aabb8eea9ab7a09beff8430b6cd4928ebcb8bd89
Andreas Obkircher <andreas.obkircher@rubicon.eu>
2010-03-09 10:51:53 +0000

    implemented ResolvingExpressionVisitor.VisitSqlEntityRefMemberExpression added test (RM-2411)

--- Remotion/Data/Linq.UnitTests/SqlBackend/MappingResolution/ResolvingExpressionVisitorTest.cs
+++ Remotion/Data/Linq.UnitTests/SqlBackend/MappingResolution/ResolvingExpressionVisitorTest.cs
@@ -20,6 +20,7 @@ using NUnit.Framework;
 using NUnit.Framework.SyntaxHelpers;
 using Remotion.Data.Linq.SqlBackend.MappingResolution;
 using Remotion.Data.Linq.SqlBackend.SqlStatementModel;
+using Remotion.Data.Linq.SqlBackend.SqlStatementModel.Resolved;
 using Remotion.Data.Linq.SqlBackend.SqlStatementModel.Unresolved;
 using Remotion.Data.Linq.UnitTests.SqlBackend.SqlStatementModel;
 using Remotion.Data.Linq.UnitTests.TestDomain;
@@ -67,7 +68,7 @@ namespace Remotion.Data.Linq.UnitTests.SqlBackend.MappingResolution
       var unresolvedResult = new SqlTableReferenceExpression (_sqlTable);
       var resolvedResult = Expression.Constant (0);
 
-      using (_resolverMock.GetMockRepository ().Ordered ())
+      using (_resolverMock.GetMockRepository().Ordered())
       {
         _resolverMock
             .Expect (mock => mock.ResolveTableReferenceExpression (tableReferenceExpression))
@@ -80,7 +81,7 @@ namespace Remotion.Data.Linq.UnitTests.SqlBackend.MappingResolution
       var result = ResolvingExpressionVisitor.ResolveExpression (tableReferenceExpression, _resolverMock, _generator);
 
       Assert.That (result, Is.SameAs (resolvedResult));
-      _resolverMock.VerifyAllExpectations ();
+      _resolverMock.VerifyAllExpectations();
     }
 
     [Test]
@@ -94,11 +95,11 @@ namespace Remotion.Data.Linq.UnitTests.SqlBackend.MappingResolution
       var result = ResolvingExpressionVisitor.ResolveExpression (tableReferenceExpression, _resolverMock, _generator);
 
       Assert.That (result, Is.SameAs (tableReferenceExpression));
-      _resolverMock.VerifyAllExpectations ();
+      _resolverMock.VerifyAllExpectations();
     }
 
     [Test]
-    public void VisitSqlMemberExpression_CreatesSqlColumnExpression() 
+    public void VisitSqlMemberExpression_CreatesSqlColumnExpression ()
     {
       var memberInfo = typeof (Cook).GetProperty ("Substitution");
       var memberExpression = new SqlMemberExpression (_sqlTable, memberInfo);
@@ -112,7 +113,7 @@ namespace Remotion.Data.Linq.UnitTests.SqlBackend.MappingResolution
       var result = ResolvingExpressionVisitor.ResolveExpression (memberExpression, _resolverMock, _generator);
 
       Assert.That (result, Is.SameAs (fakeResult));
-      _resolverMock.VerifyAllExpectations ();
+      _resolverMock.VerifyAllExpectations();
     }
 
     [Test]
@@ -121,13 +122,13 @@ namespace Remotion.Data.Linq.UnitTests.SqlBackend.MappingResolution
       var memberInfo = typeof (Cook).GetProperty ("FirstName");
       var sqlMemberExpression = new SqlMemberExpression (_sqlTable, memberInfo);
 
-      var unresolvedResult = new SqlMemberExpression(_sqlTable, memberInfo);
+      var unresolvedResult = new SqlMemberExpression (_sqlTable, memberInfo);
       var resolvedResult = Expression.Constant (0);
 
-      using (_resolverMock.GetMockRepository ().Ordered ())
+      using (_resolverMock.GetMockRepository().Ordered())
       {
         _resolverMock
-            .Expect (mock => mock.ResolveMemberExpression(sqlMemberExpression, _generator))
+            .Expect (mock => mock.ResolveMemberExpression (sqlMemberExpression, _generator))
             .Return (unresolvedResult);
         _resolverMock
             .Expect (mock => mock.ResolveMemberExpression (unresolvedResult, _generator))
@@ -137,7 +138,7 @@ namespace Remotion.Data.Linq.UnitTests.SqlBackend.MappingResolution
       var result = ResolvingExpressionVisitor.ResolveExpression (sqlMemberExpression, _resolverMock, _generator);
 
       Assert.That (result, Is.SameAs (resolvedResult));
-      _resolverMock.VerifyAllExpectations ();
+      _resolverMock.VerifyAllExpectations();
     }
 
     [Test]
@@ -145,15 +146,42 @@ namespace Remotion.Data.Linq.UnitTests.SqlBackend.MappingResolution
     {
       var memberInfo = typeof (Cook).GetProperty ("FirstName");
       var sqlMemberExpression = new SqlMemberExpression (_sqlTable, memberInfo);
-      
+
       _resolverMock
-          .Expect (mock => mock.ResolveMemberExpression(sqlMemberExpression, _generator))
+          .Expect (mock => mock.ResolveMemberExpression (sqlMemberExpression, _generator))
           .Return (sqlMemberExpression);
 
       var result = ResolvingExpressionVisitor.ResolveExpression (sqlMemberExpression, _resolverMock, _generator);
 
       Assert.That (result, Is.SameAs (sqlMemberExpression));
-      _resolverMock.VerifyAllExpectations ();
+      _resolverMock.VerifyAllExpectations();
+    }
+
+    [Test]
+    public void VisitSqlEntityRefMemberExpression_ResolvesExpression ()
+    {
+      var memberInfo = typeof (Cook).GetProperty ("Substitution");
+      var sqlEntityRefMemberExpression = new SqlEntityRefMemberExpression (_sqlTable, memberInfo);
+      var primaryColumn = new SqlColumnExpression (typeof (int), "c", "ID");
+      var foreignColumn = new SqlColumnExpression (typeof (int), "s", "ID");
+      var tableSource = new SqlTableSource (typeof(Cook), "CookTable", "s");
+      var expectedSqlJoinedTableSource = new SqlJoinedTableSource (tableSource, primaryColumn, foreignColumn);
+
+      using (_resolverMock.GetMockRepository ().Ordered ())
+      {
+        _resolverMock
+            .Expect (mock => mock.ResolveJoinedTableSource (Arg<JoinedTableSource>.Is.Anything))
+            .Return (expectedSqlJoinedTableSource);
+
+        _resolverMock
+            .Expect (mock => mock.ResolveTableReferenceExpression (Arg<SqlTableReferenceExpression>.Is.Anything))
+            .Return (new SqlColumnListExpression (typeof (Cook)));
+      }
+
+      var result = ResolvingExpressionVisitor.ResolveExpression (sqlEntityRefMemberExpression, _resolverMock, _generator);
+
+      Assert.That (result, Is.TypeOf (typeof (SqlColumnListExpression)));
+      _resolverMock.VerifyAllExpectations();
     }
 
     [Test]
