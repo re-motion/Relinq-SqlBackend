d244a3459f31a6374d627667c67bcd88f474c91e
Andreas Obkircher <andreas.obkircher@rubicon.eu>
2010-03-31 11:32:22 +0000

    added SqlFunctionExpression (RM-2511)

---
+++ Remotion/Data/Linq.SqlBackend/SqlStatementModel/SqlSpecificExpressions/SqlFunctionExpression.cs
@@ -0,0 +1,73 @@
+// This file is part of the re-motion Core Framework (www.re-motion.org)
+// Copyright (C) 2005-2009 rubicon informationstechnologie gmbh, www.rubicon.eu
+// 
+// The re-motion Core Framework is free software; you can redistribute it 
+// and/or modify it under the terms of the GNU Lesser General Public License 
+// as published by the Free Software Foundation; either version 2.1 of the 
+// License, or (at your option) any later version.
+// 
+// re-motion is distributed in the hope that it will be useful, 
+// but WITHOUT ANY WARRANTY; without even the implied warranty of 
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 
+// GNU Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License
+// along with re-motion; if not, see http://www.gnu.org/licenses.
+// 
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using Remotion.Data.Linq.Clauses.Expressions;
+using Remotion.Data.Linq.Parsing;
+using Remotion.Data.Linq.Utilities;
+
+namespace Remotion.Data.Linq.SqlBackend.SqlStatementModel.SqlSpecificExpressions
+{
+  /// <summary>
+  /// <see cref="SqlFunctionExpression"/> holds the sql specific function with its parameters.
+  /// </summary>
+  public class SqlFunctionExpression : ExtensionExpression
+  {
+    private string _sqlFunctioName;
+    private Expression[] _args;
+
+    public SqlFunctionExpression (Type type, string sqlFunctioName, params Expression[] args)
+        : base (type)
+    {
+      ArgumentUtility.CheckNotNull ("type", type);
+      ArgumentUtility.CheckNotNull ("sqlFunctioName", sqlFunctioName);
+
+      _args = args;
+      _sqlFunctioName = sqlFunctioName;
+    }
+
+    public string SqlFunctioName
+    {
+      get { return _sqlFunctioName; }
+    }
+
+    public Expression[] Args
+    {
+      get { return _args; }
+    }
+
+    protected override Expression VisitChildren (ExpressionTreeVisitor visitor)
+    {
+      var newArgList = new List<Expression>();
+
+      foreach (var expression in Args)
+        newArgList.Add (visitor.VisitExpression (expression));
+
+      if (_args.ToList() != newArgList)
+        return new SqlFunctionExpression (Type, _sqlFunctioName, newArgList.ToArray());
+      else
+        return this;
+    }
+
+    public override Expression Accept (ExpressionTreeVisitor visitor)
+    {
+      throw new NotImplementedException();
+    }
+  }
+}
---
+++ Remotion/Data/Linq.UnitTests/Linq/SqlBackend/SqlStatementModel/SqlSpecificExpressions/SqlFunctionExpressionTest.cs
@@ -0,0 +1,61 @@
+// This file is part of the re-motion Core Framework (www.re-motion.org)
+// Copyright (C) 2005-2009 rubicon informationstechnologie gmbh, www.rubicon.eu
+// 
+// The re-motion Core Framework is free software; you can redistribute it 
+// and/or modify it under the terms of the GNU Lesser General Public License 
+// as published by the Free Software Foundation; either version 2.1 of the 
+// License, or (at your option) any later version.
+// 
+// re-motion is distributed in the hope that it will be useful, 
+// but WITHOUT ANY WARRANTY; without even the implied warranty of 
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 
+// GNU Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License
+// along with re-motion; if not, see http://www.gnu.org/licenses.
+// 
+using System;
+using System.Linq.Expressions;
+using NUnit.Framework;
+using NUnit.Framework.SyntaxHelpers;
+using Remotion.Data.Linq.Parsing;
+using Remotion.Data.Linq.SqlBackend.SqlStatementModel.SqlSpecificExpressions;
+using Remotion.Data.Linq.UnitTests.Linq.Core.Clauses.Expressions;
+using Rhino.Mocks;
+
+namespace Remotion.Data.Linq.UnitTests.Linq.SqlBackend.SqlStatementModel.SqlSpecificExpressions
+{
+  [TestFixture]
+  public class SqlFunctionExpressionTest
+  {
+    private SqlFunctionExpression _sqlFunctionExpression;
+
+    [SetUp]
+    public void SetUp ()
+    {
+      _sqlFunctionExpression = new SqlFunctionExpression (typeof (string), "Test", Expression.Constant (1), Expression.Constant (2));
+    }
+
+    [Test]
+    public void VisitChildren ()
+    {
+      var visitorMock = MockRepository.GenerateStrictMock<ExpressionTreeVisitor> ();
+      var expression = Expression.Constant (3);
+
+      visitorMock
+          .Expect (mock => mock.VisitExpression (_sqlFunctionExpression.Args[0]))
+          .Return (expression);
+      visitorMock
+          .Expect (mock => mock.VisitExpression (_sqlFunctionExpression.Args[1]))
+          .Return (expression);
+
+      var result = ExtensionExpressionTestHelper.CallVisitChildren (_sqlFunctionExpression, visitorMock);
+
+      visitorMock.VerifyAllExpectations ();
+
+      Assert.That (result, Is.Not.SameAs (_sqlFunctionExpression));
+    }
+
+
+  }
+}
