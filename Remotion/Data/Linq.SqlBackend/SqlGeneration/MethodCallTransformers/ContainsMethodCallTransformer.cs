144894c8d1edb97825eba3f604412ceffee80bc5
Matthias Goertler <matthias.goertler@rubicon.eu>
2010-03-31 15:22:00 +0000

    implemented ContainsMethodCallTransformer (RM-2511)

---
+++ Remotion/Data/Linq.SqlBackend/SqlGeneration/MethodCallTransformers/ContainsMethodCallTransformer.cs
@@ -0,0 +1,38 @@
+// This file is part of the re-motion Core Framework (www.re-motion.org)
+// Copyright (C) 2005-2009 rubicon informationstechnologie gmbh, www.rubicon.eu
+// 
+// The re-motion Core Framework is free software; you can redistribute it 
+// and/or modify it under the terms of the GNU Lesser General Public License 
+// as published by the Free Software Foundation; either version 2.1 of the 
+// License, or (at your option) any later version.
+// 
+// re-motion is distributed in the hope that it will be useful, 
+// but WITHOUT ANY WARRANTY; without even the implied warranty of 
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 
+// GNU Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License
+// along with re-motion; if not, see http://www.gnu.org/licenses.
+// 
+using System;
+using System.Linq.Expressions;
+using Remotion.Data.Linq.SqlBackend.SqlStatementModel.Unresolved;
+using Remotion.Data.Linq.Utilities;
+
+namespace Remotion.Data.Linq.SqlBackend.SqlGeneration.MethodCallTransformers
+{
+  /// <summary>
+  /// <see cref="ContainsMethodCallTransformer"/> implements <see cref="IMethodCallTransformer"/> for the contains method.
+  /// </summary>
+  public class ContainsMethodCallTransformer : IMethodCallTransformer
+  {
+    public Expression Transform (MethodCallExpression methodCallExpression)
+    {
+      ArgumentUtility.CheckNotNull ("methodCallExpression", methodCallExpression);
+
+      var rightExpression = Expression.Constant (string.Format ("%{0}%", methodCallExpression.Arguments[0]));
+
+      return new SqlBinaryOperatorExpression ("LIKE", methodCallExpression.Object, rightExpression);
+    }
+  }
+}
