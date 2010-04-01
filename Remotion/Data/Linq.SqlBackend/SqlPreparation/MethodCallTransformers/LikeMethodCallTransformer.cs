059a12534a683d3e1c35096cf7c808129bf5a612
Andreas Obkircher <andreas.obkircher@rubicon.eu>
2010-04-01 13:35:38 +0000

    added LikeMethodCallTransformer added tests (RM-2509)

---
+++ Remotion/Data/Linq.SqlBackend/SqlPreparation/MethodCallTransformers/LikeMethodCallTransformer.cs
@@ -0,0 +1,46 @@
+// This file is part of the re-motion Core Framework (www.re-motion.org)
+// Copyright (C) 2005-2009 rubicon informationstechnologie gmbh, www.rubicon.eu
+// 
+// The re-motion Core Framework is free software; you can redistribute it 
+// and/or modify it under the terms of the GNU Lesser General Public License 
+// as published by the Free Software Foundation; either version 2.1 of the 
+// License, or (at your option) any later version.
+// 
+// re-motion is distributed in the hope that it will be useful, 
+// but WITHOUT ANY WARRANTY; without even the implied warranty of 
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 
+// GNU Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License
+// along with re-motion; if not, see http://www.gnu.org/licenses.
+// 
+using System;
+using System.Linq.Expressions;
+using System.Reflection;
+using Remotion.Data.Linq.SqlBackend.SqlGeneration;
+using Remotion.Data.Linq.SqlBackend.SqlStatementModel.Unresolved;
+using Remotion.Data.Linq.Utilities;
+
+namespace Remotion.Data.Linq.SqlBackend.SqlPreparation.MethodCallTransformers
+{
+  /// <summary>
+  /// <see cref="LikeMethodCallTransformer"/> implements <see cref="IMethodCallTransformer"/> for the like extension method.
+  /// </summary>
+  public class LikeMethodCallTransformer : IMethodCallTransformer
+  {
+    public static readonly MethodInfo[] SupportedMethods = new MethodInfo[] { };
+
+    public Expression Transform (MethodCallExpression methodCallExpression)
+    {
+      ArgumentUtility.CheckNotNull ("methodCallExpression", methodCallExpression);
+
+      if (!(methodCallExpression.Arguments[0] is ConstantExpression))
+        throw new NotSupportedException ("Only expressions that can be evaluated locally can be used as the argument for Contains.");
+
+      var rightExpression =
+          Expression.Constant (string.Format ("'{0}'", ((ConstantExpression) methodCallExpression.Arguments[1]).Value));
+
+      return new SqlBinaryOperatorExpression ("LIKE", methodCallExpression.Object, rightExpression);
+    }
+  }
+}
