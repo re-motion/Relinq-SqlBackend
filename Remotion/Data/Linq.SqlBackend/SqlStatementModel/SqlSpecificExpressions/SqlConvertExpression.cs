73145607f61ec8703a078aa0983896d481713958
Andreas Obkircher <andreas.obkircher@rubicon.eu>
2010-04-02 07:20:14 +0000

    added SqlConvertExpression added tests (RM-2510)

---
+++ Remotion/Data/Linq.SqlBackend/SqlStatementModel/SqlSpecificExpressions/SqlConvertExpression.cs
@@ -0,0 +1,71 @@
+// This file is part of the re-motion Core Framework (www.re-motion.org)
+// Copyright (C) 2005-2009 rubicon informationstechnologie gmbh, www.rubicon.eu
+// 
+// The re-motion Core Framework is free software; you can redistribute it 
+// and/or modify it under the terms of the GNU Lesser General Public License 
+// as published by the Free Software Foundation; either version 2.1 of the 
+// License, or (at your option) any later version.
+// 
+// re-motion is distributed in the hope that it will be useful, 
+// but WITHOUT ANY WARRANTY; without even the implied warranty of 
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 
+// GNU Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License
+// along with re-motion; if not, see http://www.gnu.org/licenses.
+// 
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using Remotion.Data.Linq.Clauses.Expressions;
+using Remotion.Data.Linq.Parsing;
+using Remotion.Data.Linq.Utilities;
+
+namespace Remotion.Data.Linq.SqlBackend.SqlStatementModel.SqlSpecificExpressions
+{
+  /// <summary>
+  /// <see cref="SqlConvertExpression"/> is used to represent a convert expression.
+  /// </summary>
+  public class SqlConvertExpression : ExtensionExpression
+  {
+    private readonly Expression _source;
+    private Dictionary<Type, string> _sqlTypeMapper;
+
+    public SqlConvertExpression (Type targetType, Expression source)
+        : base (targetType)
+    {
+      ArgumentUtility.CheckNotNull ("source", source);
+
+      _source = source;
+      _sqlTypeMapper = new Dictionary<Type, string>
+                       {
+                           { typeof (string), "nvarchar" },
+                           { typeof (int), "int" },
+                           { typeof (bool), "bit" },
+                       };
+    }
+
+    public Expression Source
+    {
+      get { return _source; }
+    }
+
+    public string GetSqlTypeName ()
+    {
+      if (_sqlTypeMapper.ContainsKey (Type))
+        return _sqlTypeMapper[Type];
+      else
+        throw new KeyNotFoundException (string.Format("No appropriate mapper for type '{0}' found.", Type.Name));
+    }
+
+    protected override Expression VisitChildren (ExpressionTreeVisitor visitor)
+    {
+      return this;
+    }
+
+    public override Expression Accept (ExpressionTreeVisitor visitor)
+    {
+      throw new NotImplementedException();
+    }
+  }
+}
---
+++ Remotion/Data/Linq.UnitTests/Linq/SqlBackend/SqlStatementModel/SqlSpecificExpressions/SqlConvertExpressionTest.cs
@@ -0,0 +1,69 @@
+// This file is part of the re-motion Core Framework (www.re-motion.org)
+// Copyright (C) 2005-2009 rubicon informationstechnologie gmbh, www.rubicon.eu
+// 
+// The re-motion Core Framework is free software; you can redistribute it 
+// and/or modify it under the terms of the GNU Lesser General Public License 
+// as published by the Free Software Foundation; either version 2.1 of the 
+// License, or (at your option) any later version.
+// 
+// re-motion is distributed in the hope that it will be useful, 
+// but WITHOUT ANY WARRANTY; without even the implied warranty of 
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 
+// GNU Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License
+// along with re-motion; if not, see http://www.gnu.org/licenses.
+// 
+using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+using NUnit.Framework.SyntaxHelpers;
+using Remotion.Data.Linq.Parsing;
+using Remotion.Data.Linq.SqlBackend.SqlStatementModel.SqlSpecificExpressions;
+using System.Linq.Expressions;
+using Remotion.Data.Linq.UnitTests.Linq.Core.Clauses.Expressions;
+using Remotion.Data.Linq.UnitTests.Linq.Core.TestDomain;
+using Rhino.Mocks;
+
+namespace Remotion.Data.Linq.UnitTests.Linq.SqlBackend.SqlStatementModel.SqlSpecificExpressions
+{
+  [TestFixture]
+  public class SqlConvertExpressionTest
+  {
+    private SqlConvertExpression _convertExpresion;
+
+    [SetUp]
+    public void SetUp ()
+    {
+      _convertExpresion = new SqlConvertExpression (typeof (int), Expression.Constant ("1"));
+    }
+
+    [Test]
+    public void GetSqlTypeName ()
+    {
+      var result = _convertExpresion.GetSqlTypeName();
+      Assert.That (result, Is.EqualTo ("int"));
+    }
+
+    [Test]
+    [ExpectedException (typeof (KeyNotFoundException), ExpectedMessage = "No appropriate mapper for type 'Cook' found.")]
+    public void GetSqlTypeName_KeyNotFound_ThrowsException ()
+    {
+      var convertExpression = new SqlConvertExpression (typeof (Cook), Expression.Constant ("1"));
+      convertExpression.GetSqlTypeName ();
+    }
+
+    [Test]
+    public void VisitChildren_ReturnsThis ()
+    {
+      var visitorMock = MockRepository.GenerateStrictMock<ExpressionTreeVisitor> ();
+      visitorMock.Replay ();
+
+      var result = ExtensionExpressionTestHelper.CallVisitChildren (_convertExpresion, visitorMock);
+
+      visitorMock.VerifyAllExpectations ();
+      Assert.That (result, Is.SameAs (_convertExpresion));
+    }
+    
+  }
+}
