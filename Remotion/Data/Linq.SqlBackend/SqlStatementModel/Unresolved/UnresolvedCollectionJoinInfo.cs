017504010843808d9239bb83bb65c26c0f76881c
Andreas Obkircher <andreas.obkircher@rubicon.eu>
2010-04-16 08:46:17 +0000

    added UnresolvedCollectionJoinInfo (RM-2615)

---
+++ Remotion/Data/Linq.SqlBackend/SqlStatementModel/Unresolved/UnresolvedCollectionJoinInfo.cs
@@ -0,0 +1,67 @@
+// This file is part of the re-motion Core Framework (www.re-motion.org)
+// Copyright (C) 2005-2009 rubicon informationstechnologie gmbh, www.rubicon.eu
+// 
+// The re-motion Core Framework is free software; you can redistribute it 
+// and/or modify it under the terms of the GNU Lesser General Public License 
+// as published by the Free Software Foundation; either version 2.1 of the 
+// License, or (at your option) any later version.
+// 
+// re-motion is distributed in the hope that it will be useful, 
+// but WITHOUT ANY WARRANTY; without even the implied warranty of 
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 
+// GNU Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License
+// along with re-motion; if not, see http://www.gnu.org/licenses.
+// 
+using System;
+using Remotion.Data.Linq.SqlBackend.SqlStatementModel.Resolved;
+using System.Linq.Expressions;
+using System.Reflection;
+using Remotion.Data.Linq.Utilities;
+
+namespace Remotion.Data.Linq.SqlBackend.SqlStatementModel.Unresolved
+{
+  /// <summary>
+  /// <see cref="UnresolvedCollectionJoinInfo"/> represents the data source defined by a member access to a collection in a LINQ expression.
+  /// </summary>
+  public class UnresolvedCollectionJoinInfo : IJoinInfo
+  {
+    private readonly Expression _sourceExpression;
+    private readonly MemberInfo _memberInfo;
+
+    public UnresolvedCollectionJoinInfo (Expression sourceExpression, MemberInfo memberInfo)
+    {
+      ArgumentUtility.CheckNotNull ("sourceExpression", sourceExpression);
+      ArgumentUtility.CheckNotNull ("memberInfo", memberInfo);
+      
+      _memberInfo = memberInfo;
+      _sourceExpression = sourceExpression;
+    }
+
+    public Expression SourceExpression
+    {
+      get { return _sourceExpression; }
+    }
+
+    public MemberInfo MemberInfo
+    {
+      get { return _memberInfo; }
+    }
+
+    public Type ItemType
+    {
+      get { throw new NotImplementedException(); }
+    }
+
+    public IJoinInfo Accept (IJoinInfoVisitor visitor)
+    {
+      throw new NotImplementedException();
+    }
+
+    public ResolvedJoinInfo GetResolvedJoinInfo ()
+    {
+       throw new InvalidOperationException ("This join has not yet been resolved; call the resolution step first.");
+    }
+  }
+}
