bd578bc5784d2169ea6f6a39720eee721882fbda
Andreas Obkircher <andreas.obkircher@rubicon.eu>
2010-04-16 13:08:15 +0000

    added missing test (RM-2564)

---
+++ Remotion/Data/Linq.UnitTests/Linq/SqlBackend/SqlGeneration/TestableSqlCustomTextGeneratorExpression.cs
@@ -0,0 +1,43 @@
+// This file is part of the re-motion Core Framework (www.re-motion.org)
+// Copyright (C) 2005-2009 rubicon informationstechnologie gmbh, www.rubicon.eu
+// 
+// The re-motion Core Framework is free software; you can redistribute it 
+// and/or modify it under the terms of the GNU Lesser General Public License 
+// as published by the Free Software Foundation; either version 2.1 of the 
+// License, or (at your option) any later version.
+// 
+// re-motion is distributed in the hope that it will be useful, 
+// but WITHOUT ANY WARRANTY; without even the implied warranty of 
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 
+// GNU Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License
+// along with re-motion; if not, see http://www.gnu.org/licenses.
+// 
+using System;
+using System.Linq.Expressions;
+using Remotion.Data.Linq.Parsing;
+using Remotion.Data.Linq.SqlBackend.SqlGeneration;
+
+namespace Remotion.Data.Linq.UnitTests.Linq.SqlBackend.SqlGeneration
+{
+  public class TestableSqlCustomTextGeneratorExpression : SqlCustomTextGeneratorExpressionBase
+  {
+    public TestableSqlCustomTextGeneratorExpression (Type expressionType)
+        : base(expressionType)
+    {
+    }
+
+    public override void Generate (ISqlCommandBuilder commandBuilder, ExpressionTreeVisitor textGeneratingExpressionVisitor, ISqlGenerationStage stage)
+    {
+      throw new NotImplementedException();
+    }
+
+
+    protected override Expression VisitChildren (ExpressionTreeVisitor visitor)
+    {
+      throw new NotImplementedException();
+    }
+
+   }
+}
