e4b04f6077004878b450d4153c23b495e5d66206
Andreas Obkircher <andreas.obkircher@rubicon.eu>
2010-04-29 08:38:49 +0000

    added AnyResultOperatorHandler (RM-2664)

---
+++ Remotion/Data/Linq.SqlBackend/SqlPreparation/ResultOperatorHandlers/AnyResultOperatorHandler.cs
@@ -0,0 +1,40 @@
+// This file is part of the re-motion Core Framework (www.re-motion.org)
+// Copyright (C) 2005-2009 rubicon informationstechnologie gmbh, www.rubicon.eu
+// 
+// The re-motion Core Framework is free software; you can redistribute it 
+// and/or modify it under the terms of the GNU Lesser General Public License 
+// as published by the Free Software Foundation; either version 2.1 of the 
+// License, or (at your option) any later version.
+// 
+// re-motion is distributed in the hope that it will be useful, 
+// but WITHOUT ANY WARRANTY; without even the implied warranty of 
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 
+// GNU Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License
+// along with re-motion; if not, see http://www.gnu.org/licenses.
+// 
+using System;
+using Remotion.Data.Linq.Clauses.ResultOperators;
+using Remotion.Data.Linq.SqlBackend.SqlStatementModel;
+using Remotion.Data.Linq.SqlBackend.SqlStatementModel.SqlSpecificExpressions;
+
+namespace Remotion.Data.Linq.SqlBackend.SqlPreparation.ResultOperatorHandlers
+{
+  /// <summary>
+  /// Handles the <see cref="AnyResultOperatorHandler"/>.
+  /// </summary>
+  public class AnyResultOperatorHandler : ResultOperatorHandler<AnyResultOperator>
+  {
+    protected override void HandleResultOperator (AnyResultOperator resultOperator, ref SqlStatementBuilder sqlStatementBuilder, UniqueIdentifierGenerator generator, ISqlPreparationStage stage)
+    {
+      var sqlSubStatement = GetStatementAndResetBuilder (ref sqlStatementBuilder);
+      var subStatementExpression = new SqlSubStatementExpression (sqlSubStatement);
+
+      var sqlExistsExpression = new SqlExistsExpression (subStatementExpression);
+
+      sqlStatementBuilder.SelectProjection = sqlExistsExpression;
+      sqlStatementBuilder.DataInfo = resultOperator.GetOutputDataInfo (sqlSubStatement.DataInfo);
+    }
+  }
+}
