0b25dcf56154f48c5e823d6ce4879f6c8cad6489
Andreas Obkircher <andreas.obkircher@rubicon.eu>
2010-05-11 07:41:28 +0000

    added NamedExpression added INamedExpressionVisitor (RM-2717)

---
+++ Remotion/Data/Linq.SqlBackend/SqlStatementModel/NamedExpression.cs
@@ -0,0 +1,76 @@
+// This file is part of the re-motion Core Framework (www.re-motion.org)
+// Copyright (C) 2005-2009 rubicon informationstechnologie gmbh, www.rubicon.eu
+// 
+// The re-motion Core Framework is free software; you can redistribute it 
+// and/or modify it under the terms of the GNU Lesser General Public License 
+// as published by the Free Software Foundation; either version 2.1 of the 
+// License, or (at your option) any later version.
+// 
+// re-motion is distributed in the hope that it will be useful, 
+// but WITHOUT ANY WARRANTY; without even the implied warranty of 
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 
+// GNU Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License
+// along with re-motion; if not, see http://www.gnu.org/licenses.
+// 
+using System;
+using System.Linq.Expressions;
+using Remotion.Data.Linq.Clauses.Expressions;
+using Remotion.Data.Linq.Parsing;
+using Remotion.Data.Linq.SqlBackend.SqlStatementModel.Unresolved;
+using Remotion.Data.Linq.Utilities;
+
+namespace Remotion.Data.Linq.SqlBackend.SqlStatementModel
+{
+  /// <summary>
+  /// <see cref="NamedExpression"/> holds an expression and a name for it.
+  /// </summary>
+  public class NamedExpression : ExtensionExpression
+  {
+    private readonly string _name;
+    private readonly Expression _expression;
+
+    public NamedExpression (string name, Expression expression)
+        : base(expression.Type)
+    {
+      ArgumentUtility.CheckNotNullOrEmpty ("name", name);
+      ArgumentUtility.CheckNotNull ("expression", expression);
+
+      _name = name;
+      _expression = expression;
+    }
+
+    public string Name
+    {
+      get { return _name; }
+    }
+
+    public Expression Expression
+    {
+      get { return _expression; }
+    }
+
+    protected override Expression VisitChildren (ExpressionTreeVisitor visitor)
+    {
+      var newExpression = visitor.VisitExpression (_expression);
+      if (newExpression != _expression)
+        return new NamedExpression(_name, newExpression);
+      else
+        return this;
+    }
+
+    public override Expression Accept (ExpressionTreeVisitor visitor)
+    {
+      ArgumentUtility.CheckNotNull ("visitor", visitor);
+
+      var specificVisitor = visitor as INamedExpressionVisitor;
+      if (specificVisitor != null)
+        return specificVisitor.VisitNamedExpression (this);
+      else
+        return base.Accept (visitor);
+    }
+  }
+
+  
+}
---
+++ Remotion/Data/Linq.UnitTests/Linq/SqlBackend/SqlStatementModel/NamedExpressionTest.cs
@@ -0,0 +1,99 @@
+// This file is part of the re-motion Core Framework (www.re-motion.org)
+// Copyright (C) 2005-2009 rubicon informationstechnologie gmbh, www.rubicon.eu
+// 
+// The re-motion Core Framework is free software; you can redistribute it 
+// and/or modify it under the terms of the GNU Lesser General Public License 
+// as published by the Free Software Foundation; either version 2.1 of the 
+// License, or (at your option) any later version.
+// 
+// re-motion is distributed in the hope that it will be useful, 
+// but WITHOUT ANY WARRANTY; without even the implied warranty of 
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 
+// GNU Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License
+// along with re-motion; if not, see http://www.gnu.org/licenses.
+// 
+using System;
+using System.Linq.Expressions;
+using NUnit.Framework;
+using NUnit.Framework.SyntaxHelpers;
+using Remotion.Data.Linq.Parsing;
+using Remotion.Data.Linq.SqlBackend.SqlStatementModel;
+using Remotion.Data.Linq.UnitTests.Linq.Core.Clauses.Expressions;
+using Remotion.Data.Linq.UnitTests.Linq.Core.TestDomain;
+using Rhino.Mocks;
+
+namespace Remotion.Data.Linq.UnitTests.Linq.SqlBackend.SqlStatementModel
+{
+  [TestFixture]
+  public class NamedExpressionTest
+  {
+    private NamedExpression _namedExpression;
+    private ConstantExpression _wrappredExpression;
+
+    [SetUp]
+    public void SetUp ()
+    {
+      _wrappredExpression = Expression.Constant (1);
+      _namedExpression = new NamedExpression ("test", _wrappredExpression);
+    }
+
+    [Test]
+    public void Initialization ()
+    {
+      Assert.That (_namedExpression.Type, Is.EqualTo (typeof (int)));
+    }
+
+    [Test]
+    public void VisitChildren_ReturnsSameExpression ()
+    {
+      var visitorMock = MockRepository.GenerateStrictMock<ExpressionTreeVisitor> ();
+
+      visitorMock
+          .Expect (mock => mock.VisitExpression (_wrappredExpression))
+          .Return (_wrappredExpression);
+      visitorMock.Replay ();
+
+      var result = ExtensionExpressionTestHelper.CallVisitChildren (_namedExpression, visitorMock);
+
+      visitorMock.VerifyAllExpectations ();
+      Assert.That (result, Is.SameAs (_namedExpression));
+    }
+
+    [Test]
+    public void VisitChildren_ReturnsNewSqlInExpression ()
+    {
+      var newExpression = Expression.Constant (5);
+      var visitorMock = MockRepository.GenerateStrictMock<ExpressionTreeVisitor> ();
+      
+      visitorMock
+          .Expect (mock => mock.VisitExpression (_wrappredExpression))
+          .Return (newExpression);
+      visitorMock.Replay ();
+
+      var result = ExtensionExpressionTestHelper.CallVisitChildren (_namedExpression, visitorMock);
+
+      visitorMock.VerifyAllExpectations ();
+      Assert.That (result, Is.Not.SameAs (_namedExpression));
+      Assert.That (((NamedExpression) result).Expression, Is.SameAs (newExpression));
+      Assert.That (((NamedExpression) result).Name, Is.EqualTo("test"));
+    }
+
+    [Test]
+    public void Accept_VisitorSupportingExpressionType ()
+    {
+      ExtensionExpressionTestHelper.CheckAcceptForVisitorSupportingType<NamedExpression, INamedExpressionVisitor> (
+          _namedExpression,
+          mock => mock.VisitNamedExpression(_namedExpression));
+    }
+
+    [Test]
+    public void Accept_VisitorNotSupportingExpressionType ()
+    {
+      ExtensionExpressionTestHelper.CheckAcceptForVisitorNotSupportingType (_namedExpression);
+    }
+
+
+  }
+}
