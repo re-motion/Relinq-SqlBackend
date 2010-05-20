0a999cf0f72b751028effc95318b7cc551a6ff69
Andreas Obkircher <andreas.obkircher@rubicon.eu>
2010-05-20 14:58:04 +0000

    added FromExpressionInfo (RM-2774)

---
+++ Remotion/Data/Linq.SqlBackend/SqlPreparation/FromExpressionInfo.cs
@@ -0,0 +1,45 @@
+// This file is part of the re-motion Core Framework (www.re-motion.org)
+// Copyright (C) 2005-2009 rubicon informationstechnologie gmbh, www.rubicon.eu
+// 
+// The re-motion Core Framework is free software; you can redistribute it 
+// and/or modify it under the terms of the GNU Lesser General Public License 
+// as published by the Free Software Foundation; either version 2.1 of the 
+// License, or (at your option) any later version.
+// 
+// re-motion is distributed in the hope that it will be useful, 
+// but WITHOUT ANY WARRANTY; without even the implied warranty of 
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 
+// GNU Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License
+// along with re-motion; if not, see http://www.gnu.org/licenses.
+// 
+using System;
+using System.Collections.ObjectModel;
+using Remotion.Data.Linq.Clauses;
+using Remotion.Data.Linq.SqlBackend.SqlStatementModel;
+using Remotion.Data.Linq.Utilities;
+
+namespace Remotion.Data.Linq.SqlBackend.SqlPreparation
+{
+  /// <summary>
+  /// <see cref="FromExpressionInfo"/> is returned by <see cref="SqlPreparationFromExpressionVisitor.GetTableForFromExpression"/>.
+  /// </summary>
+  public struct FromExpressionInfo
+  {
+    public FromExpressionInfo (SqlTableBase sqlTable, Ordering[] extractedOrderings, IQuerySource itemSelector)
+    {
+      ArgumentUtility.CheckNotNull ("sqlTable", sqlTable);
+      ArgumentUtility.CheckNotNull ("extractedOrderings", extractedOrderings);
+      ArgumentUtility.CheckNotNull ("itemSelector", itemSelector);
+
+      SqlTable = sqlTable;
+      ExtractedOrderings = Array.AsReadOnly(extractedOrderings);
+      ItemSelector = itemSelector;
+    }
+
+    public readonly SqlTableBase SqlTable;
+    public readonly ReadOnlyCollection<Ordering> ExtractedOrderings;
+    public readonly IQuerySource ItemSelector;
+  }
+}
