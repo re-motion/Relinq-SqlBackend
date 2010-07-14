c1a8d874fc6c9160ea63c480a833faeb3a4eaaa5
Fabian Schmied <fabian.schmied@rubicon.eu>
2010-07-14 08:51:28 +0000

    Test commit

--- Remotion/Data/Linq.UnitTests/Linq/SqlBackend/MappingResolution/DefaultMappingResolutionStageTest.cs
+++ Remotion/Data/Linq.UnitTests/Linq/SqlBackend/MappingResolution/DefaultMappingResolutionStageTest.cs
@@ -14,6 +14,7 @@
 // You should have received a copy of the GNU Lesser General Public License
 // along with re-motion; if not, see http://www.gnu.org/licenses.
 // 
+
 using System;
 using System.Linq.Expressions;
 using NUnit.Framework;
