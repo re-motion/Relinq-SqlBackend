fb28103320fdb096b615790b92eae1504e419960
Sascha Gindele <sascha.gindele@rubicon.eu>
2010-08-04 08:21:43 +0000

    TestResultChecker start

---
+++ Remotion/Data/Linq.LinqToSqlAdapter/TestResultChecker.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Remotion.Data.Linq.LinqToSqlAdapter
+{
+  public class TestResultChecker
+  {
+  }
+}
---
+++ Remotion/Data/Linq.UnitTests/LinqToSqlAdapter/TestResultCheckerTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using Remotion.Data.Linq.LinqToSqlAdapter;
+
+namespace Remotion.Data.Linq.UnitTests.LinqToSqlAdapter
+{
+  [TestFixture]
+  class TestResultCheckerTest
+  {
+    
+  }
+}
