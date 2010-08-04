1096817d53cfbf45e122b174121b8fdaee3cb2a2
Sascha Gindele <sascha.gindele@rubicon.eu>
2010-08-04 09:55:41 +0000

    TestResultChecker

--- Remotion/Data/Linq.UnitTests/LinqToSqlAdapter/TestResultCheckerTest.cs
+++ Remotion/Data/Linq.UnitTests/LinqToSqlAdapter/TestResultCheckerTest.cs
@@ -1,15 +1,49 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿// This file is part of the re-motion Core Framework (www.re-motion.org)
+// Copyright (C) 2005-2009 rubicon informationstechnologie gmbh, www.rubicon.eu
+// 
+// The re-motion Core Framework is free software; you can redistribute it 
+// and/or modify it under the terms of the GNU Lesser General Public License 
+// as published by the Free Software Foundation; either version 2.1 of the 
+// License, or (at your option) any later version.
+// 
+// re-motion is distributed in the hope that it will be useful, 
+// but WITHOUT ANY WARRANTY; without even the implied warranty of 
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 
+// GNU Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License
+// along with re-motion; if not, see http://www.gnu.org/licenses.
+// 
+using System;
 using NUnit.Framework;
+using NUnit.Framework.SyntaxHelpers;
 using Remotion.Data.Linq.LinqToSqlAdapter;
 
 namespace Remotion.Data.Linq.UnitTests.LinqToSqlAdapter
 {
   [TestFixture]
-  class TestResultCheckerTest
+  public class TestResultCheckerTest
   {
-    
+    [Test]
+    public void Check_SimpleValue ()
+    {
+      var test_expected = "should be";
+      var test_actual = "should be";
+
+      var result = TestResultChecker.Check (test_expected, test_actual);
+
+      Assert.That (result, Is.EqualTo (true));
+    }
+
+    [Test]
+    public void Check_SimpleValueFalse ()
+    {
+      var test_expected = "should be";
+      var test_actual = "";
+
+      var result = TestResultChecker.Check (test_expected, test_actual);
+
+      Assert.That (result, Is.EqualTo (false));
+    }
   }
 }
