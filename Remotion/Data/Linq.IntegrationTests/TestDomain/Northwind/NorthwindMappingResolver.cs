23b080da22e51761e55603763029c296e4c2cbf3
Patrick Schwarz <patrick.schwarz@rubicon.eu>
2010-08-09 10:31:58 +0000

    MappingResolverTests created and partially implemented - TestResolveTableInfo - TestResolveSimpleTableInfo - TestMetaModelMapping

---
+++ Remotion/Data/Linq.IntegrationTests/TestDomain/Northwind/NorthwindMappingResolver.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Linq.Mapping;
+using System.Linq.Expressions;
+using System.Reflection;
+using Remotion.Data.Linq.SqlBackend.MappingResolution;
+using Remotion.Data.Linq.SqlBackend.SqlStatementModel.Resolved;
+using Remotion.Data.Linq.SqlBackend.SqlStatementModel.Unresolved;
+using Remotion.Data.Linq.Utilities;
+
+namespace Remotion.Data.Linq.IntegrationTests.TestDomain.Northwind
+{
+  public class NorthwindMappingResolver:IMappingResolver
+  {
+    private readonly MetaModel _metaModel;
+    
+
+    public NorthwindMappingResolver()
+    {
+        _metaModel = new AttributeMappingSource ().GetModel (typeof (Northwind));
+    }
+
+    public IResolvedTableInfo ResolveTableInfo (UnresolvedTableInfo tableInfo, UniqueIdentifierGenerator generator)
+    {
+      MetaTable table = _metaModel.GetTable (tableInfo.ItemType);
+
+      return new ResolvedSimpleTableInfo (tableInfo.ItemType, table.TableName, generator.GetUniqueIdentifier("t"));
+    }
+
+    public ResolvedJoinInfo ResolveJoinInfo (UnresolvedJoinInfo joinInfo, UniqueIdentifierGenerator generator)
+    {
+      throw new NotImplementedException();
+    }
+
+    //TODO: Better Solution For IResolveTableInfo? (Current: Cast to SimpleTableInfo) 
+    public SqlEntityDefinitionExpression ResolveSimpleTableInfo (IResolvedTableInfo tableInfo, UniqueIdentifierGenerator generator)
+    {
+      ArgumentUtility.CheckNotNull ("simpleTableInfo", tableInfo);
+      ResolvedSimpleTableInfo simpleTableInfo = (ResolvedSimpleTableInfo) tableInfo;
+      
+
+      SqlColumnExpression primaryColumn=null;
+      List<SqlColumnExpression> otherColumns=new List<SqlColumnExpression>();
+
+      MetaTable table = _metaModel.GetTable (tableInfo.ItemType);
+      foreach (var metaDataMember in table.RowType.DataMembers)
+      {
+        SqlColumnExpression sqlColumnExpression = new SqlColumnDefinitionExpression (
+            metaDataMember.Type, simpleTableInfo.TableAlias, metaDataMember.MappedName, metaDataMember.IsPrimaryKey);
+
+        if (metaDataMember.IsPrimaryKey)
+          primaryColumn = sqlColumnExpression;
+        else
+          otherColumns.Add (sqlColumnExpression);
+      }
+
+      return new SqlEntityDefinitionExpression (
+          simpleTableInfo.ItemType, simpleTableInfo.TableAlias, simpleTableInfo.TableName, primaryColumn, otherColumns.ToArray());
+    }
+
+    public Expression ResolveMemberExpression (SqlEntityExpression originatingEntity, MemberInfo memberInfo)
+    {
+      throw new NotImplementedException();
+    }
+
+    public Expression ResolveMemberExpression (SqlColumnExpression sqlColumnExpression, MemberInfo memberInfo)
+    {
+      throw new NotImplementedException();
+    }
+
+    public Expression ResolveConstantExpression (ConstantExpression constantExpression)
+    {
+      throw new NotImplementedException();
+    }
+
+    public Expression ResolveTypeCheck (Expression expression, Type desiredType)
+    {
+      throw new NotImplementedException();
+    }
+  }
+}
---
+++ Remotion/Data/Linq.IntegrationTests/UnitTests/MappingResolverTests.cs
@@ -0,0 +1,81 @@
+﻿using System.Data.Linq;
+using System.Data.Linq.Mapping;
+using NUnit.Framework;
+using Remotion.Data.Linq.IntegrationTests.TestDomain.Northwind;
+using Remotion.Data.Linq.SqlBackend.MappingResolution;
+using Remotion.Data.Linq.SqlBackend.SqlStatementModel.Resolved;
+using Remotion.Data.Linq.SqlBackend.SqlStatementModel.Unresolved;
+
+namespace Remotion.Data.Linq.IntegrationTests.UnitTests
+{
+  [TestFixture]
+  public class MappingResolverTests
+  {
+    private UniqueIdentifierGenerator _generator;
+    private IMappingResolver _mappingResolver;
+
+    [SetUp]
+    public void SetUp()
+    {
+       _generator=new UniqueIdentifierGenerator();
+       _mappingResolver = new NorthwindMappingResolver ();
+    }
+
+    [Test]
+    public void TestMetaModelMapping()
+    {
+      System.Data.Linq.Mapping.MappingSource mappingSource = new AttributeMappingSource ();
+
+      var table = mappingSource.GetModel (typeof (Northwind)).GetTable (typeof (Customer));
+      Assert.AreEqual ("dbo.Customers",table.TableName);
+
+      string companyName = "CompanyName";
+
+      string expectedType = "NVarChar(40) NOT NULL";
+      string resolvedType=string.Empty;
+      
+      foreach (var metaDataMember in table.RowType.DataMembers)
+      {
+        if(!metaDataMember.Name.Equals (companyName))
+          continue;
+
+        resolvedType = metaDataMember.DbType;
+      }
+
+      Assert.AreEqual (expectedType, resolvedType);
+    }
+
+    [Test]
+    public void TestResolveTableInfo()
+    {
+      UnresolvedTableInfo unresolvedTableInfo = new UnresolvedTableInfo (typeof(Customer));
+      
+      ResolvedSimpleTableInfo resolvedTableInfo = (ResolvedSimpleTableInfo) _mappingResolver.ResolveTableInfo (unresolvedTableInfo, _generator);
+
+      ResolvedSimpleTableInfo simpleTableInfo=new ResolvedSimpleTableInfo (typeof(Customer),"dbo.Customers","t0");
+      
+      Assert.AreEqual (simpleTableInfo.ItemType, resolvedTableInfo.ItemType);
+      Assert.AreEqual (simpleTableInfo.TableAlias, resolvedTableInfo.TableAlias);
+      Assert.AreEqual (simpleTableInfo.TableName, resolvedTableInfo.TableName);
+    }
+
+    [Test]
+    public void TestResolveSimpleTableInfo()
+    {
+      ResolvedSimpleTableInfo simpleTableInfo = new ResolvedSimpleTableInfo (typeof (Region), "dbo.Region", "t0");
+
+      SqlColumnExpression primaryColumn = new SqlColumnDefinitionExpression(typeof(int), simpleTableInfo.TableAlias, "RegionID", true);
+      SqlColumnExpression descriptionColumn = new SqlColumnDefinitionExpression(typeof(string),simpleTableInfo.TableAlias, "RegionDescription", false);
+      SqlColumnExpression territoriesColumn = new SqlColumnDefinitionExpression(typeof(EntitySet<Territory>),simpleTableInfo.TableAlias, "Territories", false);
+
+      SqlEntityDefinitionExpression expectedExpr = new SqlEntityDefinitionExpression (simpleTableInfo.ItemType, simpleTableInfo.TableAlias, simpleTableInfo.TableName, primaryColumn, descriptionColumn, territoriesColumn);
+
+      SqlEntityDefinitionExpression resolvedExpr = _mappingResolver.ResolveSimpleTableInfo (simpleTableInfo, _generator);
+
+      Assert.AreEqual (expectedExpr.Type, resolvedExpr.Type);
+      Assert.AreEqual (expectedExpr.TableAlias, resolvedExpr.TableAlias);
+      Assert.AreEqual (expectedExpr.PrimaryKeyColumn, resolvedExpr.PrimaryKeyColumn);
+      Assert.AreEqual (expectedExpr.Columns, resolvedExpr.Columns);
+    }
+  }
+}
