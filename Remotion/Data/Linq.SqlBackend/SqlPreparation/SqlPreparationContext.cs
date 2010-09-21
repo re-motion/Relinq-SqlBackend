a4afab02ab56334928adcf717900686f62a047fb
Fabian Schmied <fabian.schmied@rubicon.eu>
2010-09-21 10:13:26 +0000

    Cleaned up

--- Remotion/Data/Linq.SqlBackend/SqlPreparation/SqlPreparationContext.cs
+++ Remotion/Data/Linq.SqlBackend/SqlPreparation/SqlPreparationContext.cs
@@ -17,9 +17,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq.Expressions;
-using Remotion.Data.Linq.Clauses.StreamedData;
 using Remotion.Data.Linq.SqlBackend.SqlStatementModel;
-using Remotion.Data.Linq.SqlBackend.SqlStatementModel.Resolved;
 using Remotion.Data.Linq.Utilities;
 
 namespace Remotion.Data.Linq.SqlBackend.SqlPreparation
