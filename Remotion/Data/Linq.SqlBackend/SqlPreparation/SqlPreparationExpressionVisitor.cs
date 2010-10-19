25a987671fe32e7a72436fde5c8fb668315ea86f
Andreas Obkircher <andreas.obkircher@rubicon.eu>
2010-10-19 09:05:23 +0000

    refactored SqlPreparationExpressionVisitor.VisitMemberExpression to use PropertyInfo.GetGetMethod to get the property accessor (RM-3005)

--- Remotion/Data/Linq.SqlBackend/SqlPreparation/SqlPreparationExpressionVisitor.cs
+++ Remotion/Data/Linq.SqlBackend/SqlPreparation/SqlPreparationExpressionVisitor.cs
@@ -177,9 +177,7 @@ namespace Remotion.Data.Linq.SqlBackend.SqlPreparation
       var memberAsPropertyInfo = expression.Member as PropertyInfo;
       if (memberAsPropertyInfo != null)
       {
-        // TODO Review 3005: Use propertyInfo.GetGetMethod() to access the property getter; use that instead of GetMethod ("get_Length")! Add a unit test showing that it works with other properties than string.Length.
-
-        var methodInfo = expression.Member.DeclaringType.GetMethod ("get_Length");
+        var methodInfo = memberAsPropertyInfo.GetGetMethod();
         if (methodInfo != null)
         {
           var methodCallExpression = Expression.Call (expression.Expression, methodInfo);
