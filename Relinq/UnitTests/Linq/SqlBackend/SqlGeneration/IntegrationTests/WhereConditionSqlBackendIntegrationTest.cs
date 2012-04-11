33e99fd551cdfbb4b48e6456fe2ea338d85e55ff
Fabian Schmied <fabian.schmied@rubicon.eu>
2012-04-11 07:28:23 +0000

    Extracted DelegateInvocationSqlBackendIntegrationTest from WhereConditionSqlBackendIntegrationTest (RM-4769)

--- Relinq/UnitTests/Linq/SqlBackend/SqlGeneration/IntegrationTests/WhereConditionSqlBackendIntegrationTest.cs
+++ Relinq/UnitTests/Linq/SqlBackend/SqlGeneration/IntegrationTests/WhereConditionSqlBackendIntegrationTest.cs
@@ -16,9 +16,7 @@
 // 
 using System;
 using System.Linq;
-using System.Linq.Expressions;
 using NUnit.Framework;
-using Remotion.Linq.UnitTests.Linq.Core.TestDomain;
 using Remotion.Linq.SqlBackend.SqlGeneration;
 
 namespace Remotion.Linq.UnitTests.Linq.SqlBackend.SqlGeneration.IntegrationTests
@@ -64,36 +62,5 @@ namespace Remotion.Linq.UnitTests.Linq.SqlBackend.SqlGeneration.IntegrationTests
           new CommandParameter ("@1", "hugo")
           );
     }
-
-    [Test]
-    public void InvocationExpression_Inline ()
-    {
-      CheckQuery (
-          Cooks.Where (c => ((Func<Cook, bool>) (c1 => c1.Name != null)) (c)).Select (c => c.FirstName),
-          "SELECT [t0].[FirstName] AS [value] FROM [CookTable] AS [t0] WHERE ([t0].[Name] IS NOT NULL)");
-    }
-
-    [Test]
-    public void InvocationExpression_WithCustomExpressions ()
-    {
-      Expression<Func<Cook, bool>> predicate1 = c => c.ID > 100;
-      Expression<Func<Cook, bool>> predicate2 = c => c.Name != null;
-
-      // c => c.ID > 100 && ((c1 => c1.Name != null) (c))
-      var combinedPredicate = 
-          Expression.Lambda<Func<Cook, bool>> (
-              Expression.AndAlso (
-                  predicate1.Body,
-                  Expression.Invoke (predicate2, predicate1.Parameters.Cast<Expression>())
-              ),
-          predicate1.Parameters);
-      
-      CheckQuery (
-          Cooks.Where (combinedPredicate).Select (c => c.FirstName),
-          "SELECT [t0].[FirstName] AS [value] FROM [CookTable] AS [t0] WHERE (([t0].[ID] > @1) AND ([t0].[Name] IS NOT NULL))",
-          new CommandParameter ("@1", 100)
-          );
-    }
-
   }
 }
