333a1c89a6b182fb08f9fdc80ed187d22955f2c9
Fabian Schmied <fabian.schmied@rubicon.eu>
2014-11-06 13:55:36 +0100

    RMLNQSQL-30 Added SetOperation and SetOperationCombinedStatement.

---
+++ Core/SqlStatementModel/SetOperation.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Remotion.Linq.SqlBackend.SqlStatementModel
+{
+  /// <summary>
+  /// Identifies a SQL set operation.
+  /// </summary>
+  public enum SetOperation
+  {
+    Union
+  }
+}
