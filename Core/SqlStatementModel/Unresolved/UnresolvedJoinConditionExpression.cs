83966143c0b98b06473ca713a78eb80e3b68cb53
Fabian Schmied <fabian.schmied@rubicon.eu>
2014-11-13 11:54:46 +0100

    RMLNQSQL-64 Added UnresolvedJoinConditionExpression.

---
+++ Core/SqlStatementModel/Unresolved/UnresolvedJoinConditionExpression.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq.Expressions;
+using System.Reflection;
+using Remotion.Linq.Clauses.Expressions;
+using Remotion.Linq.Parsing;
+using Remotion.Linq.SqlBackend.SqlStatementModel.Resolved;
+using Remotion.Utilities;
+
+namespace Remotion.Linq.SqlBackend.SqlStatementModel.Unresolved
+{
+  // TODO RMLNQSQL-64: Test
+  /// <summary>
+  /// Represents a yet unresolved join condition.
+  /// </summary>
+  public class UnresolvedJoinConditionExpression : ExtensionExpression
+  {
+    private readonly SqlTable _joinedTable;
+
+    public UnresolvedJoinConditionExpression (SqlEntityExpression originatingEntity, MemberInfo memberInfo, SqlTable joinedTable)
+        : base (typeof (bool))
+    {
+      ArgumentUtility.CheckNotNull ("joinedTable", joinedTable);
+      _joinedTable = joinedTable;
+    }
+
+
+  // TODO RMLNQSQL-64: Implement.
+    protected override Expression VisitChildren (ExpressionTreeVisitor visitor)
+    {
+      throw new NotImplementedException();
+    }
+  }
+}
