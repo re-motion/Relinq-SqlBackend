fc57d21578ea619c01287a6786a09437503af6ed
Michael Ketting <michael.ketting@rubicon.eu>
2015-03-29 09:41:01 +0200

    RMLNQSQL-86 Renamed build scripts

--- Build.cmd
+++ Build.cmd
@@ -1,10 +1,11 @@
 @echo off
 pushd %~dp0
-set msbuild="C:\Windows\Microsoft.NET\Framework\v4.0.30319\msbuild.exe"
-set log-dir=Build\BuildOutput\log
-set nuget-bin=Build\BuildOutput\temp\nuget-bin
+set msbuild="C:\Program Files (x86)\MSBuild\12.0\Bin\MSBuild.exe"
+set log-dir=build\BuildOutput\log
+set nuget-bin=build\BuildOutput\temp\nuget-bin
 set nuget=%nuget-bin%\nuget.exe
 set nuget-download=powershell.exe -NoProfile -Command "& {(New-Object System.Net.WebClient).DownloadFile('https://www.nuget.org/nuget.exe','%nuget%')}"
+set solutionFile=Relinq-SqlBackend.sln
 
 if not exist remotion.snk goto nosnk
 
@@ -15,28 +16,30 @@ echo.
 echo Choose your desired build:
 echo [1] ... Test build ^(x86-debug^)
 echo [2] ... Full build ^(x86-debug/release, x64-debug/release^)
-echo [3] ... Docs build ^(x86-debug if not present, docs^)
+rem echo [3] ... Quick build ^(x86-debug, no tests are run^)
+echo [4] ... Docs build ^(x86-debug if not present, docs^)
 echo           Requires Sandcastle Help File Builder to be installed!
-echo [4] ... Package ^(create zip package from pre-existent build^)
-echo [5] ... Run DependDB
-echo [6] ... Oops, nothing please - exit.
+echo [5] ... Package ^(create zip package from pre-existent build^)
+echo [6] ... Run DependDB
+echo [7] ... Oops, nothing please - exit.
 echo.
 
-choice /c:123456 /n /m "Your choice: "
+choice /c:1234567 /n /m "Your choice: "
 
 if %ERRORLEVEL%==1 goto run_test_build
 if %ERRORLEVEL%==2 goto run_full_build
-if %ERRORLEVEL%==3 goto run_docs_build
-if %ERRORLEVEL%==4 goto run_pkg_build
-if %ERRORLEVEL%==5 goto run_dependdb
-if %ERRORLEVEL%==6 goto run_exit
+if %ERRORLEVEL%==3 goto run_quick_build
+if %ERRORLEVEL%==4 goto run_docs_build
+if %ERRORLEVEL%==5 goto run_pkg_build
+if %ERRORLEVEL%==6 goto run_dependdb
+if %ERRORLEVEL%==7 goto run_exit
 goto build_succeeded
 
 :run_test_build
 mkdir %log-dir%
 mkdir %nuget-bin%
 %nuget-download%
-%nuget% restore . -NonInteractive
+%nuget% restore %solutionFile% -NonInteractive
 %msbuild% build\Remotion.Local.build /t:TestBuild /maxcpucount /verbosity:normal /flp:verbosity=normal;logfile=build\BuildOutput\log\build.log
 if not %ERRORLEVEL%==0 goto build_failed
 goto build_succeeded
@@ -45,8 +48,17 @@ goto build_succeeded
 mkdir %log-dir%
 mkdir %nuget-bin%
 %nuget-download%
-%nuget% restore . -NonInteractive
+%nuget% restore %solutionFile% -NonInteractive
 %msbuild% build\Remotion.Local.build /t:FullBuildWithoutDocumentation /maxcpucount /verbosity:normal /flp:verbosity=normal;logfile=build\BuildOutput\log\build.log
+if not %ERRORLEVEL%==0 goto build_failed
+goto build_succeeded
+
+:run_quick_build
+mkdir %log-dir%
+mkdir %nuget-bin%
+%nuget-download%
+%nuget% restore %solutionFile% -NonInteractive
+%msbuild% build\Remotion.Local.build /t:QuickBuild /maxcpucount /verbosity:normal /flp:verbosity=normal;logfile=build\BuildOutput\log\build.log
 if not %ERRORLEVEL%==0 goto build_failed
 goto build_succeeded
 
@@ -54,7 +66,7 @@ goto build_succeeded
 mkdir %log-dir%
 mkdir %nuget-bin%
 %nuget-download%
-%nuget% restore . -NonInteractive
+%nuget% restore %solutionFile% -NonInteractive
 %msbuild% build\Remotion.Local.build /t:DocumentationBuild /maxcpucount /verbosity:minimal /flp:verbosity=normal;logfile=build\BuildOutput\log\build.log
 if not %ERRORLEVEL%==0 goto build_failed
 goto build_succeeded
@@ -63,7 +75,7 @@ goto build_succeeded
 mkdir %log-dir%
 mkdir %nuget-bin%
 %nuget-download%
-%nuget% restore . -NonInteractive
+%nuget% restore %solutionFile% -NonInteractive
 %msbuild% build\Remotion.Local.build /t:PackageBuild /maxcpucount /verbosity:minimal /flp:verbosity=normal;logfile=build\BuildOutput\log\build.log
 if not %ERRORLEVEL%==0 goto build_failed
 goto build_succeeded
@@ -72,7 +84,7 @@ goto build_succeeded
 mkdir %log-dir%
 mkdir %nuget-bin%
 %nuget-download%
-%nuget% restore . -NonInteractive
+%nuget% restore %solutionFile% -NonInteractive
 %msbuild% build\Remotion.Local.build /t:DependDBBuild /maxcpucount /verbosity:normal /flp:verbosity=detailed;logfile=build\BuildOutput\log\build.log
 if not %ERRORLEVEL%==0 goto build_failed
 goto build_succeeded
