59ce05023edcd8e9ebaa393ca24d8b90aecda2bd
Michael Ketting <michael.ketting@rubicon.eu>
2015-11-10 11:19:21 +0100

    RMLNQSQL-77 Introduce NullJoinConditionExpression to represent joins that do not have a condition. This condition must always be optimized.

---
+++ Core/SqlStatementModel/Unresolved/NullJoinConditionExpression.cs
@@ -0,0 +1,60 @@
+﻿// This file is part of the re-linq project (relinq.codeplex.com)
+// Copyright (c) rubicon IT GmbH, www.rubicon.eu
+// 
+// re-linq is free software; you can redistribute it and/or modify it under 
+// the terms of the GNU Lesser General Public License as published by the 
+// Free Software Foundation; either version 2.1 of the License, 
+// or (at your option) any later version.
+// 
+// re-linq is distributed in the hope that it will be useful, 
+// but WITHOUT ANY WARRANTY; without even the implied warranty of 
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 
+// GNU Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License
+// along with re-linq; if not, see http://www.gnu.org/licenses.
+// 
+
+using System;
+using System.Linq.Expressions;
+using Remotion.Utilities;
+
+namespace Remotion.Linq.SqlBackend.SqlStatementModel.Unresolved
+{
+  /// <summary>
+  /// Represents a join condition that is always <b>true</b> and can thus be optimized.
+  /// </summary>
+  public class NullJoinConditionExpression : Expression
+  {
+    public NullJoinConditionExpression ()
+    {
+    }
+
+    public override ExpressionType NodeType
+    {
+      get { return ExpressionType.Extension; }
+    }
+
+    public override Type Type
+    {
+      get { return typeof (bool); }
+    }
+
+    protected override Expression VisitChildren (ExpressionVisitor visitor)
+    {
+      ArgumentUtility.CheckNotNull ("visitor", visitor);
+      return this;
+    }
+
+    protected override Expression Accept (ExpressionVisitor visitor)
+    {
+      ArgumentUtility.CheckNotNull ("visitor", visitor);
+      return base.Accept (visitor);
+    }
+
+    public override string ToString ()
+    {
+      return "NullJoinCondition()";
+    }
+  }
+}
---
+++ UnitTests/SqlStatementModel/Unresolved/NullJoinConditionExpressionTest.cs
@@ -0,0 +1,81 @@
+﻿// This file is part of the re-linq project (relinq.codeplex.com)
+// Copyright (c) rubicon IT GmbH, www.rubicon.eu
+// 
+// re-linq is free software; you can redistribute it and/or modify it under 
+// the terms of the GNU Lesser General Public License as published by the 
+// Free Software Foundation; either version 2.1 of the License, 
+// or (at your option) any later version.
+// 
+// re-linq is distributed in the hope that it will be useful, 
+// but WITHOUT ANY WARRANTY; without even the implied warranty of 
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 
+// GNU Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License
+// along with re-linq; if not, see http://www.gnu.org/licenses.
+// 
+using System;
+using System.Linq.Expressions;
+using Moq;
+using NUnit.Framework;
+using Remotion.Linq.SqlBackend.SqlStatementModel.Unresolved;
+
+namespace Remotion.Linq.SqlBackend.UnitTests.SqlStatementModel.Unresolved
+{
+  [TestFixture]
+  public class NullJoinConditionExpressionTest
+  {
+    private NullJoinConditionExpression _nullJoinConditionExpression;
+
+    [SetUp]
+    public void SetUp ()
+    {
+      _nullJoinConditionExpression = new NullJoinConditionExpression ();
+    }
+
+    [Test]
+    public void NodeType ()
+    {
+      Assert.That (_nullJoinConditionExpression.NodeType, Is.EqualTo (ExpressionType.Extension));
+    }
+
+    [Test]
+    public void Type ()
+    {
+      Assert.That (_nullJoinConditionExpression.Type, Is.EqualTo (typeof (bool)));
+    }
+
+    [Test]
+    public void VisitChildren_ReturnsThis_WithoutCallingVisitMethods ()
+    {
+      var visitorMock = new Mock<ExpressionVisitor> (MockBehavior.Strict);
+
+      var result = ExtensionExpressionTestHelper.CallVisitChildren (_nullJoinConditionExpression, visitorMock.Object);
+
+      Assert.That (result, Is.SameAs (_nullJoinConditionExpression));
+      visitorMock.Verify();
+    }
+
+    [Test]
+    public void Accept_VisitorNeverSupportsThisType ()
+    {
+      ExtensionExpressionTestHelper.CheckAcceptForVisitorNotSupportingType (_nullJoinConditionExpression);
+    }
+
+    [Test]
+    public void GetResolvedTableInfo ()
+    {
+      Assert.That (
+          () => UnresolvedDummyRowTableInfo.Instance.GetResolvedTableInfo(),
+          Throws.InvalidOperationException.With.Message.EqualTo ("This table has not yet been resolved; call the resolution step first."));
+    }
+
+    [Test]
+    public void To_String ()
+    {
+      var result = _nullJoinConditionExpression.ToString();
+
+      Assert.That (result, Is.EqualTo ("NullJoinCondition()"));
+    } 
+  }
+}
