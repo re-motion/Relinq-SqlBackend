7ec37b094ac99f5c9f719a7e030861aef0de4c4c
Stefan Ilic <stefan.ilic@rubicon.eu>
2020-07-30 12:40:22 +0200

    Restore original line breaks

--- UnitTests/SqlStatementModel/SqlSpecificExpressions/SqlFunctionExpressionTest.cs
+++ UnitTests/SqlStatementModel/SqlSpecificExpressions/SqlFunctionExpressionTest.cs
@@ -41,18 +41,9 @@ namespace Remotion.Linq.SqlBackend.UnitTests.SqlStatementModel.SqlSpecificExpres
       var visitorMock = new Mock<ExpressionVisitor> (MockBehavior.Strict);
       var newArgs = new[] { Expression.Constant (1), Expression.Constant (8), Expression.Constant (9) };
       
-      visitorMock
-          .Setup (mock => mock.Visit (_sqlFunctionExpression.Args[0]))
-          .Returns (newArgs[0])
-          .Verifiable();
-      visitorMock
-          .Setup (mock => mock.Visit (_sqlFunctionExpression.Args[1]))
-          .Returns (newArgs[1])
-          .Verifiable();
-      visitorMock
-          .Setup (mock => mock.Visit (_sqlFunctionExpression.Args[2]))
-          .Returns (newArgs[2])
-          .Verifiable();
+      visitorMock.Setup (mock => mock.Visit (_sqlFunctionExpression.Args[0])).Returns (newArgs[0]).Verifiable();
+      visitorMock.Setup (mock => mock.Visit (_sqlFunctionExpression.Args[1])).Returns (newArgs[1]).Verifiable();
+      visitorMock.Setup (mock => mock.Visit (_sqlFunctionExpression.Args[2])).Returns (newArgs[2]).Verifiable();
 
       var result = ExtensionExpressionTestHelper.CallVisitChildren (_sqlFunctionExpression, visitorMock.Object);
 
@@ -68,18 +59,9 @@ namespace Remotion.Linq.SqlBackend.UnitTests.SqlStatementModel.SqlSpecificExpres
     {
       var visitorMock = new Mock<ExpressionVisitor> (MockBehavior.Strict);
       
-      visitorMock
-          .Setup (mock => mock.Visit (_sqlFunctionExpression.Args[0]))
-          .Returns (_sqlFunctionExpression.Args[0])
-          .Verifiable();
-      visitorMock
-          .Setup (mock => mock.Visit (_sqlFunctionExpression.Args[1]))
-          .Returns (_sqlFunctionExpression.Args[1])
-          .Verifiable();
-      visitorMock
-          .Setup (mock => mock.Visit (_sqlFunctionExpression.Args[2]))
-          .Returns (_sqlFunctionExpression.Args[2])
-          .Verifiable();
+      visitorMock.Setup (mock => mock.Visit (_sqlFunctionExpression.Args[0])).Returns (_sqlFunctionExpression.Args[0]).Verifiable();
+      visitorMock.Setup (mock => mock.Visit (_sqlFunctionExpression.Args[1])).Returns (_sqlFunctionExpression.Args[1]).Verifiable();
+      visitorMock.Setup (mock => mock.Visit (_sqlFunctionExpression.Args[2])).Returns (_sqlFunctionExpression.Args[2]).Verifiable();
 
       var result = ExtensionExpressionTestHelper.CallVisitChildren (_sqlFunctionExpression, visitorMock.Object);
 
