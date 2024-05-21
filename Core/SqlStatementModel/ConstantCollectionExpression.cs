c8643f2ac922dcffb13bb6fbd1da42194f143ebb
Martin Herwich <martin.herwich@rubicon.eu>
2024-05-21 11:09:05 +0200

    RMLNQSQL-173 Add ConstantCollectionExpression

---
+++ Core/SqlStatementModel/ConstantCollectionExpression.cs
@@ -0,0 +1,118 @@
+﻿// This file is part of the re-linq project (relinq.codeplex.com)
+// Copyright (c) rubicon IT GmbH, www.rubicon.eu
+//
+// re-linq is free software; you can redistribute it and/or modify it under
+// the terms of the GNU Lesser General Public License as published by the
+// Free Software Foundation; either version 2.1 of the License,
+// or (at your option) any later version.
+//
+// re-linq is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with re-linq; if not, see http://www.gnu.org/licenses.
+//
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using Remotion.Utilities;
+
+namespace Remotion.Linq.SqlBackend.SqlStatementModel
+{
+  /// <summary>
+  /// Expression representing an <see cref="ICollection"/>, an <see cref="ICollection{T}"/> or an <see cref="IReadOnlyCollection{T}"/>.
+  /// </summary>
+  public class ConstantCollectionExpression : Expression
+  {
+    /// <summary>
+    /// The collection that was the original <see cref="ConstantExpression"/>'s <see cref="ConstantExpression.Value"/>.
+    /// </summary>
+    public object Collection => _items;
+
+    /// <summary>
+    /// Indicates whether the <see cref="ConstantCollectionExpression.Collection"/> is without elements.
+    /// </summary>
+    public bool IsEmptyCollection { get; }
+
+    /// <summary>
+    /// The <see cref="System.Type"/> of the <see cref="ConstantCollectionExpression.Collection"/>.
+    /// </summary>
+    public override Type Type { get; }
+
+    private readonly IEnumerable _items;
+
+    /// <summary>
+    /// Creates a <see cref="ConstantCollectionExpression"/> for the given <paramref name="collection"/>.
+    /// </summary>
+    public ConstantCollectionExpression (IEnumerable collection)
+    {
+      ArgumentUtility.CheckNotNull (nameof(collection), collection);
+
+      // ReSharper disable PossibleMultipleEnumeration
+      _items = collection;
+      Type = collection.GetType();
+
+      var enumerator = collection.GetEnumerator();
+      IsEmptyCollection = !enumerator.MoveNext();
+      // ReSharper restore PossibleMultipleEnumeration
+
+      if (enumerator is IDisposable disposable)
+        disposable.Dispose();
+    }
+
+    public IEnumerable<object> GetItems ()
+    {
+      return _items.Cast<object>();
+    }
+
+    protected override Expression Accept (ExpressionVisitor visitor)
+    {
+      ArgumentUtility.CheckNotNull (nameof(visitor), visitor);
+
+      if (visitor is IConstantCollectionExpressionVisitor collectionExpressionVisitor)
+        return collectionExpressionVisitor.VisitConstantCollection (this);
+
+      return base.Accept (visitor);
+    }
+
+    protected override Expression VisitChildren (ExpressionVisitor visitor)
+    {
+      return this;
+    }
+
+    public override ExpressionType NodeType => ExpressionType.Constant;
+    
+    public override string ToString ()
+    {
+      var enumerator = _items.GetEnumerator();
+      var stringBuilder = new StringBuilder (1024);
+      stringBuilder.Append ('[');
+      for (int i = 0; i < 5; i++)
+      {
+        if (!enumerator.MoveNext())
+          break;
+
+        if (i > 0)
+          stringBuilder.Append (", ");
+
+        stringBuilder.Append (enumerator.Current);
+      }
+
+      if (enumerator.MoveNext())
+        stringBuilder.Append (", ...");
+
+      stringBuilder.Append (']');
+
+      if (enumerator is IDisposable disposable)
+        disposable.Dispose();
+
+      return stringBuilder.ToString();
+    }
+  }
+}
---
+++ UnitTests/SqlStatementModel/ConstantCollectionExpressionTest.cs
@@ -0,0 +1,103 @@
+﻿// This file is part of the re-linq project (relinq.codeplex.com)
+// Copyright (c) rubicon IT GmbH, www.rubicon.eu
+//
+// re-linq is free software; you can redistribute it and/or modify it under
+// the terms of the GNU Lesser General Public License as published by the
+// Free Software Foundation; either version 2.1 of the License,
+// or (at your option) any later version.
+//
+// re-linq is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with re-linq; if not, see http://www.gnu.org/licenses.
+//
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Moq;
+using NUnit.Framework;
+using Remotion.Linq.SqlBackend.SqlStatementModel;
+
+namespace Remotion.Linq.SqlBackend.UnitTests.SqlStatementModel
+{
+  [TestFixture]
+  public class ConstantCollectionExpressionTest
+  {
+    [Test]
+    public void Initialize ()
+    {
+      var collection = new[] { 4, 2 };
+
+      var result = new ConstantCollectionExpression (collection);
+
+      Assert.That (result.Collection, Is.EqualTo (collection));
+      Assert.That (result.IsEmptyCollection, Is.False);
+    }
+
+    [Test]
+    public void Initialize_WithConstantExpression_ForEmptyCollection_IsEmpty ()
+    {
+      var collection = new string[0];
+
+      var result = new ConstantCollectionExpression (collection);
+
+      Assert.That (result.Collection, Is.EqualTo (collection));
+      Assert.That (result.IsEmptyCollection, Is.True);
+    }
+
+    private static IEnumerable<IEnumerable> GetCollectionTestCasesForGetItems ()
+    {
+      var emptyCollectionStub = new Mock<ICollection>();
+      emptyCollectionStub.Setup (_ => _.GetEnumerator()).Returns (Mock.Of<IEnumerator>());
+      yield return emptyCollectionStub.Object;
+
+      yield return new[] { 17, 4, 42 };
+      yield return new[] { "One", "Two", "Three" };
+      yield return new[] { "One".ToCharArray(), "Two".ToCharArray(), "Three".ToCharArray() };
+    }
+
+    [Test]
+    [TestCaseSource(nameof(GetCollectionTestCasesForGetItems))]
+    public void GetItems (IEnumerable collection)
+    {
+      var constantCollectionExpression = new ConstantCollectionExpression (collection);
+      var result = constantCollectionExpression.GetItems();
+      Assert.That (result, Is.EqualTo (collection));
+    }
+
+    [Test]
+    public void Accept_VisitorSupportingExpressionType ()
+    {
+      var collection = new[] { 4, 2, 42 };
+      var constantCollectionExpression = new ConstantCollectionExpression (collection);
+      ExtensionExpressionTestHelper.CheckAcceptForVisitorSupportingType<ConstantCollectionExpression, IConstantCollectionExpressionVisitor> (
+          constantCollectionExpression,
+          mock => mock.VisitConstantCollection (constantCollectionExpression));
+    }
+
+    [Test]
+    public void Accept_VisitorNotSupportingExpressionType ()
+    {
+      var collection = new[] { 4, 2, 42 };
+      var constantCollectionExpression = new ConstantCollectionExpression (collection);
+      ExtensionExpressionTestHelper.CheckAcceptForVisitorNotSupportingType (constantCollectionExpression);
+    }
+
+    [CLSCompliant (false)]
+    [Test]
+    [TestCase (new object[0], "[]")]
+    [TestCase (new object[] { 1 }, "[1]")]
+    [TestCase (new object[] { 1, "two" }, "[1, two]")]
+    [TestCase (new object[] { 4, 2, 42, 17, 4 }, "[4, 2, 42, 17, 4]")]
+    [TestCase (new object[] { 4, 2, "text", 42, 17, 4 }, "[4, 2, text, 42, 17, ...]")]
+    public void ToString (object[] items, string expectedResult)
+    {
+      var expression = new ConstantCollectionExpression (items);
+      Assert.That (expression.ToString(), Is.EqualTo (expectedResult));
+    }
+  }
+}
