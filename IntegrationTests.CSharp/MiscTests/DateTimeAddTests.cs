80f15b6e5058f1ce7f07e9e364e9814d9754e68e
Patrick Widauer <patrick.widauer@rubicon.eu>
2024-07-12 10:04:09 +0200

    RMLNQSQL-170 Add support for .NET 9.0

--- IntegrationTests.CSharp/MiscTests/DateTimeAddTests.cs
+++ IntegrationTests.CSharp/MiscTests/DateTimeAddTests.cs
@@ -197,7 +197,11 @@ namespace Remotion.Linq.IntegrationTests.CSharp.MiscTests
             Seven = o.OrderDate.Value.Add (TimeSpan.FromDays (0.0001)),
             Eight = o.OrderDate.Value.Add (TimeSpan.FromDays (-0.0001)),
             Nine = o.OrderDate.Value.Add (TimeSpan.FromMilliseconds (1.5)),
+#if NET9_0_OR_GREATER
+            Ten = o.OrderDate.Value.Add (TimeSpan.FromMilliseconds (-2, 0L)), // TODO RMLNQSQL-154: Revert to '-1.5' when RMLNQSQL-154 is implemented.
+#else
             Ten = o.OrderDate.Value.Add (TimeSpan.FromMilliseconds (-2)), // TODO RMLNQSQL-154: Revert to '-1.5' when RMLNQSQL-154 is implemented.
+#endif
             Eleven = o.OrderDate.Value.Add (TimeSpan.FromMilliseconds (1500.76)),
             Twelve = o.OrderDate.Value.Add (TimeSpan.FromMilliseconds (-1500.76)),
             Thirteen = o.OrderDate.Value.Add (TimeSpan.FromMilliseconds (54987987987987.7)),
